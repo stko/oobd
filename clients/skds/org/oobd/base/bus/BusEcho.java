b522ddd64ed85898ae0dffd47807bc88762ffbee
stko <stko@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2009-10-10 09:04:04 +0000

    Dynamisches Laden von Klassen klappt jetzt

---
+++ clients/skds/org/oobd/base/bus/BusEcho.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.oobd.base.bus;
+
+/**
+ *
+ * @author steffen
+ */
+public class BusEcho extends OobdBus{
+    public BusEcho(){
+        System.out.println("Ich bin ein Bus...");
+
+    }
+
+}
