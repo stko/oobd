dddf5cb7d25dca98c356f26e8f5f04f2e817dc0f
stko <stko@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2010-03-02 20:53:06 +0000

    start of the development of OpenDiagX, a tool to convert xml based ODX dialects (Module diagnostic files)

---
+++ tools/OpenDiagX/OpenDiagX/OpenDiagX/Main.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace OpenDiagX
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.saveAsButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.outComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.saveAsButton);
+            this.panel1.Controls.Add(this.startButton);
+            this.panel1.Controls.Add(this.outComboBox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.inComboBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.fileButton);
+            this.panel1.Controls.Add(this.fileNameTextBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(435, 93);
+            this.panel1.TabIndex = 0;
+            // 
+            // saveAsButton
+            // 
+            this.saveAsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveAsButton.Location = new System.Drawing.Point(350, 64);
+            this.saveAsButton.Name = "saveAsButton";
+            this.saveAsButton.Size = new System.Drawing.Size(75, 23);
+            this.saveAsButton.TabIndex = 8;
+            this.saveAsButton.Text = "Save as..";
+            this.saveAsButton.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(268, 64);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 7;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outComboBox
+            // 
+            this.outComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.outComboBox.FormattingEnabled = true;
+            this.outComboBox.Location = new System.Drawing.Point(140, 64);
+            this.outComboBox.Name = "outComboBox";
+            this.outComboBox.Size = new System.Drawing.Size(121, 21);
+            this.outComboBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output-Transformer";
+            // 
+            // inComboBox
+            // 
+            this.inComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.inComboBox.FormattingEnabled = true;
+            this.inComboBox.Location = new System.Drawing.Point(12, 64);
+            this.inComboBox.Name = "inComboBox";
+            this.inComboBox.Size = new System.Drawing.Size(121, 21);
+            this.inComboBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input- Transformer";
+            // 
+            // fileButton
+            // 
+            this.fileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileButton.Location = new System.Drawing.Point(391, 22);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(34, 23);
+            this.fileButton.TabIndex = 2;
+            this.fileButton.Text = "...";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.fileButton_Click);
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileNameTextBox.Location = new System.Drawing.Point(12, 25);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(373, 20);
+            this.fileNameTextBox.TabIndex = 1;
+            this.fileNameTextBox.TextChanged += new System.EventHandler(this.fileNameTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.CheckFileExists = true;
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(3, 99);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(435, 240);
+            this.textBox.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 341);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "OpenDiagX";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Button saveAsButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ComboBox outComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox inComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ tools/OpenDiagX/OpenDiagX/OpenDiagX/Main.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace OpenDiagX
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void fileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog.FileName = fileNameTextBox.Text;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileNameTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //XPathDocument myXPathDoc = new XPathDocument(fileNameTextBox.Text);
+
+
+            XslCompiledTransform myXslTrans = new XslCompiledTransform();
+            //XsltSettings settings = new XsltSettings(true, false);
+            //XmlUrlResolver resolver = new XmlUrlResolver();
+            //transformer.Load(xsl, settings, resolver);
+            myXslTrans.Load("input.xslt");
+
+            XmlDocument xmlDocOut = new XmlDocument();
+            // Create a memory-based stream. 
+            byte[] storage = new byte[255];
+            MemoryStream memstrm = new MemoryStream();
+
+            // Wrap memstrm in a reader and a writer. 
+            StreamWriter memwtr = new StreamWriter(memstrm);
+            StreamReader memrdr = new StreamReader(memstrm);
+            XmlTextWriter myWriter = new XmlTextWriter(memstrm, null);
+
+            //Create a stream for output
+            //using (XmlWriter xmlWriter =
+            using (XmlWriter xmlWriter =
+            xmlDocOut.CreateNavigator().AppendChild())
+            {
+                myXslTrans.Transform(fileNameTextBox.Text, null, xmlWriter);
+            }
+
+            // Create XPathNavigator object by calling CreateNavigator of XmlDocument
+            XPathNavigator nav = xmlDocOut.CreateNavigator();
+
+
+            //Move to root node
+            nav.MoveToRoot();
+            string name = nav.Name;
+            textBox.Text+= "\r\nRoot node info: ";
+            textBox.Text += "\r\nBase URI" + nav.BaseURI.ToString();
+            textBox.Text += "\r\nName:" + nav.Name.ToString();
+            textBox.Text += "\r\nNode Type: " + nav.NodeType.ToString();
+            //textBox.Text += "\r\nNode Value: " + nav.Value.ToString();
+
+            if (nav.HasChildren)
+            {
+                nav.MoveToFirstChild();
+                textBox.Text += "\r\nChild node info: ";
+                textBox.Text += "\r\nBase URI" + nav.BaseURI.ToString();
+                textBox.Text += "\r\nName:" + nav.Name.ToString();
+                textBox.Text += "\r\nNode Type: " + nav.NodeType.ToString()+"\r\n";
+                //textBox.Text += "\r\nNode Value: " + nav.Value.ToString();
+
+                XPathNodeIterator iterator = nav.Select("/MDX/ECU_DATA/SECURITY_DATA/FIXED_BYTES/FIXED_BYTE");
+                while (iterator.MoveNext())
+                {
+                    textBox.Text += (iterator.Current.Value.ToString().Substring(2,2));
+                }
+
+            }
+
+
+
+            //Perform the actual transformation
+            xmlDocOut.WriteContentTo(myWriter);
+            myWriter.Flush();
+            memstrm.Seek(0, SeekOrigin.Begin);
+            //textBox.Text += memrdr.ReadToEnd();
+
+
+        }
+
+        private void fileNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            startButton.Enabled = File.Exists(fileNameTextBox.Text);
+        }
+
+    }
+}
