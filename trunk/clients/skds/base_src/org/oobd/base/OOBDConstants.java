6a851cb27021794b40370287c52f6d36e5865446
stko <stko@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2010-07-30 16:20:11 +0000

    Search path for dynamic loaded classes is now configurable by a props file

--- trunk/clients/skds/base_src/org/oobd/base/OOBDConstants.java
+++ trunk/clients/skds/base_src/org/oobd/base/OOBDConstants.java
@@ -9,14 +9,13 @@ package org.oobd.base;
  */
 public interface OOBDConstants {
     // Type of the different onion  messages, which are send through the system (the so called Core- messages)
+
     public static final String CM_VISUALIZE = "VISUALIZE";
     public static final String CM_CANVAS = "CANVAS";
     public static final String CM_VALUE = "VALUE";
-
     // IDs used for internal core list handling
     public static final String CL_PANE = "pane";
     public static final String CL_CANVAS = "canvas";
-
     // constants for the debug message handling
     public static final Integer DEBUG_BORING = 0;
     public static final Integer DEBUG_INFO = 1;
@@ -24,12 +23,11 @@ public interface OOBDConstants {
     public static final Integer DEBUG_ERROR = 3;
     public static final Integer DEBUG_FATAL = 4;
     public static final Integer DEBUG_DEFAULTLEVEL = DEBUG_BORING;
-
     // IDs used as field names in the onions
     public static final String FN_NAME = "name";
     public static final String FN_OWNER = "owner";
     public static final String FN_VALUESTRING = "ValueString";
-
     // Types of the different visualizer update requests
-        public static final Integer UR_USER = 0;
+    public static final Integer UR_USER = 0;
+    public final static String CorePrefsFileName = "oobdcore.props";
 }
