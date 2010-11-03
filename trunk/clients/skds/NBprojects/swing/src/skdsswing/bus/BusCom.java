0637b82263299334b2b7468bedb9069a5e2d4c22
stko <stko@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2010-11-03 17:30:31 +0000

    first functional serial port support

--- trunk/clients/skds/NBprojects/swing/src/skdsswing/bus/BusCom.java
+++ trunk/clients/skds/NBprojects/swing/src/skdsswing/bus/BusCom.java
@@ -1,4 +1,3 @@
-
 package skdsswing.bus;
 
 import java.util.logging.Level;
@@ -7,6 +6,8 @@ import org.oobd.base.*;
 import org.oobd.base.bus.*;
 import org.oobd.base.support.Onion;
 import org.json.JSONException;
+import sun.misc.*;
+
 
 import java.io.*;
 import java.io.IOException;
@@ -15,32 +16,27 @@ import javax.swing.Timer;
 //import javax.comm.*;// for SUN's serial/parallel port libraries
 import gnu.io.*; // for rxtxSerial library
 
+public class BusCom extends OobdBus implements OOBDConstants {
 
-
-public class BusCom extends OobdBus implements OOBDConstants{
-
-  static CommPortIdentifier portId;
-   static CommPortIdentifier saveportId;
-   static Enumeration        portList;
-   InputStream           inputStream;
-   SerialPort           serialPort;
-   Thread           readThread;
-
-   static String        messageString = "Hello, world!";
-   static OutputStream      outputStream;
-   static boolean        outputBufferEmptyFlag = false;
-
-
+    static CommPortIdentifier portId;
+    static CommPortIdentifier saveportId;
+    static Enumeration portList;
+    InputStream inputStream;
+    SerialPort serialPort;
+    Thread readThread;
+    static String messageString = "Hello, world!";
+    static OutputStream outputStream;
+    static boolean outputBufferEmptyFlag = false;
 
     public BusCom() {
-        Debug.msg("buscom",DEBUG_BORING,"Ich bin BusCom...");
+        Debug.msg("buscom", DEBUG_BORING, "Ich bin BusCom...");
 
     }
 
     @Override
     public void registerCore(Core thisCore) {
         super.registerCore(thisCore);
-        Debug.msg("busecho",DEBUG_BORING,"Core registered...");
+        Debug.msg("busecho", DEBUG_BORING, "Core registered...");
     }
 
     @Override
@@ -50,72 +46,131 @@ public class BusCom extends OobdBus implements OOBDConstants{
 
     public void run() {
 
-             boolean           portFound = false;
-      String           defaultPort;
-      		Properties props = new Properties();
-		try {
-			props.load(new FileInputStream("klobsserial.props"));
-		}
-catch (IOException ignored) {}
+        boolean portFound = false;
+        String defaultPort;
+        ComReader reader = new ComReader();
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream("buscom.props"));
+        } catch (IOException ignored) {
+        }
 
-      // determine the name of the serial port on several operating systems
-      String osname = System.getProperty("os.name","").toLowerCase();
-      if ( osname.startsWith("windows") ) {
-         // windows
-         defaultPort = "COM1";
-      } else if (osname.startsWith("linux")) {
-         // linux
-        defaultPort = "/dev/ttyS0";
-      } else if ( osname.startsWith("mac") ) {
-         // mac
-         defaultPort = "????";
-      } else {
-         System.out.println("Sorry, your operating system is not supported");
-         return;
-      }
+        // determine the name of the serial port on several operating systems
+        String osname = System.getProperty("os.name", "").toLowerCase();
+        if (osname.startsWith("windows")) {
+            // windows
+            defaultPort = "COM1";
+        } else if (osname.startsWith("linux")) {
+            // linux
+            defaultPort = "/dev/ttyS0";
+        } else if (osname.startsWith("mac")) {
+            // mac
+            defaultPort = "????";
+        } else {
+            System.out.println("Sorry, your operating system is not supported");
+            return;
+        }
 
-                serPortName = props.getProperty("SerialPort",defaultPort);
+        defaultPort = props.getProperty("SerialPort", defaultPort);
 
-      System.out.println("Set default port to "+defaultPort);
+        System.out.println("Set default port to " + defaultPort);
 
-		// parse ports and if the default port is found, initialized the reader
-      portList = CommPortIdentifier.getPortIdentifiers();
-      while (portList.hasMoreElements()) {
-         portId = (CommPortIdentifier) portList.nextElement();
-         if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
-            if (portId.getName().equals(defaultPort)) {
-               System.out.println("Found port: "+defaultPort);
-               portFound = true;
-               // init reader thread
-               nulltest reader = new nulltest();
+        // parse ports and if the default port is found, initialized the reader
+        // first set a workaround to find device ttyACM0 , accourding to https://bugs.launchpad.net/ubuntu/+source/rxtx/+bug/367833
+        System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+        System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM1");
+        portList = CommPortIdentifier.getPortIdentifiers();
+        while (portList.hasMoreElements()) {
+            portId = (CommPortIdentifier) portList.nextElement();
+            System.out.println("Scan port: " + portId.getName() + "ID:"+portId.getPortType());
+            if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+                if (portId.getName().equals(defaultPort)) {
+                    System.out.println("Found port: " + defaultPort);
+                    portFound = true;
+                    // init reader thread
+
+                    reader.connect(portId);
+                }
             }
-         }
 
-      }
-      if (!portFound) {
-         System.out.println("port " + defaultPort + " not found.");
-      }
-
-   }
-
-
-
-
-
+        }
+        if (!portFound) {
+            System.out.println("port " + defaultPort + " not found.");
+        }
         while (keepRunning == true) {
             Debug.msg("buscom", DEBUG_BORING, "sleeping...");
             Message msg = getMsg(true);
             Onion on = msg.getContent();
             Debug.msg("buscom", DEBUG_BORING, "Msg received:" + msg.getContent().toString());
-            try {
+            String command = on.getOnionString("command");
+            if ("serWrite".equalsIgnoreCase(command)) {
+                try {
+                    reader.write(new String(new BASE64Decoder().decodeBuffer(on.getOnionString("data"))));
+                    replyMsg(msg, new Onion(""
+                            + "{'type':'" + CM_RES_BUS + "',"
+                            + "'owner':"
+                            + "{'name':'" + getPluginName() + "'},"
+                            + "'result':'" + "" + "'}"));
+                } catch (JSONException ex) {
+                    Logger.getLogger(BusCom.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(BusCom.class.getName()).log(Level.SEVERE, null, ex);
+                }
 
-                replyMsg(msg,new Onion(""
-                        + "{'type':'" + CM_RES_BUS + "',"
-                        + "'owner':"
-                        + "{'name':'" + getPluginName() + "'},"
-                         + "'result':'" + "" + "'}"));
-            } catch (JSONException ex) {
-                Logger.getLogger(BusCom.class.getName()).log(Level.SEVERE, null, ex);
+            } else if ("serFlush".equalsIgnoreCase(command)) {
+                try {
+                    reader.flush();
+                    replyMsg(msg, new Onion(""
+                            + "{'type':'" + CM_RES_BUS + "',"
+                            + "'owner':"
+                            + "{'name':'" + getPluginName() + "'},"
+                            + "'result':'" + "" + "'}"));
+                } catch (JSONException ex) {
+                    Logger.getLogger(BusCom.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else if ("serWait".equalsIgnoreCase(command)) {
+                try {
+                    Integer result = reader.wait(new String(new BASE64Decoder().decodeBuffer(on.getOnionString("data"))), on.getInt("timeout"));
+                    System.out.println("busCom serWait: " + result);
+                    replyMsg(msg, new Onion(""
+                            + "{'type':'" + CM_RES_BUS + "',"
+                            + "'owner':"
+                            + "{'name':'" + getPluginName() + "'},"
+                            + "'result':" + result +","
+                            + "'replyID':"+on.getInt("replyID")
+                            + "}"));
+                } catch (JSONException ex) {
+                    Logger.getLogger(BusCom.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(BusCom.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+
+            } else if ("serReadLn".equalsIgnoreCase(command)) {
+                try {
+                    String result = reader.readln(on.getInt("timeout"), on.optBoolean("ignore"));
+                    System.out.println("busCom readline: " + result);
+                    replyMsg(msg, new Onion(""
+                            + "{'type':'" + CM_RES_BUS + "',"
+                            + "'owner':"
+                            + "{'name':'" + getPluginName() + "'},"
+                            + "'replyID':"+on.getInt("replyID")+ ","
+                            + "'result':'" + new BASE64Encoder().encode(result.getBytes()) + "'}"));
+                } catch (JSONException ex) {
+                    Logger.getLogger(BusCom.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            } else {
+                try {
+
+                    replyMsg(msg, new Onion(""
+                            + "{'type':'" + CM_RES_BUS + "',"
+                            + "'owner':"
+                            + "{'name':'" + getPluginName() + "'},"
+                            + "'result':'" + "" + "'}"));
+                } catch (JSONException ex) {
+                    Logger.getLogger(BusCom.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
             Debug.msg("buscom", DEBUG_BORING, "waked up after received msg...");
 
--- trunk/clients/skds/base_src/org/oobd/base/scriptengine/ScriptengineLua.java
+++ trunk/clients/skds/base_src/org/oobd/base/scriptengine/ScriptengineLua.java
@@ -6,6 +6,7 @@ package org.oobd.base.scriptengine;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Properties;
 import org.oobd.base.*;
 import org.oobd.base.support.Onion;
 
@@ -39,9 +40,9 @@ public class ScriptengineLua extends OobdScriptengine {
     private LuaCallFrame callFrame;
     private OobdScriptengine myself;
 
-    public ScriptengineLua(String ID, Core myCore) {
-        super(ID, myCore);
-        Debug.msg("scriptenginelua", DEBUG_BORING, "Ich bin der ScriptengineLua...");
+    public ScriptengineLua(String id, Core myCore) {
+        super(id, myCore);
+        Debug.msg("scriptenginelua", DEBUG_BORING, "Ich bin  ScriptengineLua mit der ID "+id);
         myself = this;
 
 
@@ -84,7 +85,7 @@ public class ScriptengineLua extends OobdScriptengine {
                     try {
                         core.transferMsg(new Message(myself, CoreMailboxName, new Onion(""
                                 + "{'type':'" + CM_CANVAS + "',"
-                                + "'owner':'" + myself.id + "',"
+                                + "'owner':'" + myself.getId() + "',"
                                 + "'name':'Canvastest_1'}")));
                     } catch (JSONException ex) {
                         Logger.getLogger(ScriptengineLua.class.getName()).log(Level.SEVERE, null, ex);
@@ -133,7 +134,7 @@ public class ScriptengineLua extends OobdScriptengine {
                         core.transferMsg(new Message(myself, CoreMailboxName, new Onion(""
                                 + "{'type':'" + CM_VISUALIZE + "',"
                                 + "'owner':"
-                                + "{'name':'" + myself.id + "'},"
+                                + "{'name':'" + myself.getId() + "'},"
                                 + "'canvas':'Canvastest_1',"
                                 + updevent
                                 + optid
@@ -188,17 +189,20 @@ public class ScriptengineLua extends OobdScriptengine {
                         answer = myself.getMsgPort().sendAndWait(new Message(myself, BusMailboxName, new Onion(""
                                 + "{'type':'" + CM_BUSTEST + "',"
                                 + "'owner':"
-                                + "{'name':'" + myself.id + "'},"
+                                + "{'name':'" + myself.getId() + "'},"
                                 + "'command':'serReadLn',"
                                 + "'timeout':'" + getInt(0) + "',"
                                 + "'ignore':'" + Boolean.toString(getBoolean(1)) + "'}")), 5000);
                         if (answer != null) {
                             try {
                                 result = new String(new BASE64Decoder().decodeBuffer(answer.getContent().getString("result")));
+                                System.out.println("Lua serReadLn result:"+result);
                             } catch (IOException ex) {
                                 Logger.getLogger(ScriptengineLua.class.getName()).log(Level.SEVERE, null, ex);
                             }
 
+                        }else{
+                            System.out.println("Lua serReadLn: No answer received");
                         }
 
                     } catch (JSONException ex) {
@@ -219,7 +223,7 @@ public class ScriptengineLua extends OobdScriptengine {
             register("serWaitCall", new JavaFunction() {
 
                 public int call(LuaCallFrame callFrame, int nArguments) {
-                    System.out.println("Lua calls serWait with strang data:>" + getString(0) + "<");
+                    System.out.println("Lua calls serWait with string data:>" + getString(0) + "<");
                     //BaseLib.luaAssert(nArguments >0, "not enough args");
                     initRPC(callFrame, nArguments);
                     int result = 0;
@@ -228,11 +232,12 @@ public class ScriptengineLua extends OobdScriptengine {
                         answer = myself.getMsgPort().sendAndWait(new Message(myself, BusMailboxName, new Onion(""
                                 + "{'type':'" + CM_BUSTEST + "',"
                                 + "'owner':"
-                                + "{'name':'" + myself.id + "'},"
+                                + "{'name':'" + myself.getId() + "'},"
                                 + "'command':'serWait',"
                                 + "'timeout':'" + getInt(1) + "',"
                                 + "'data':'" + new BASE64Encoder().encode(getString(0).getBytes()) + "'}")), 500);
                         if (answer != null) {
+                            System.out.println("Lua calls serWait returns with onion:" + answer.getContent().toString() );
                             result = answer.getContent().getInt("result");
 
                         }
@@ -275,13 +280,13 @@ public class ScriptengineLua extends OobdScriptengine {
                     //BaseLib.luaAssert(nArguments >0, "not enough args");
                     initRPC(callFrame, nArguments);
                     try {
-                        myself.getMsgPort().sendAndWait(new Message(myself, BusMailboxName, new Onion("{"
+                        core.transferMsg(new Message(myself, BusMailboxName, new Onion("{"
                                 + "'type':'" + CM_BUSTEST + "',"
                                 + "'owner':"
-                                + "{'name':'" + myself.id + "'},"
+                                + "{'name':'" + myself.getId() + "'},"
                                 + "'command':'serWrite',"
                                 + "'data':'" + new BASE64Encoder().encode(getString(0).getBytes()) + "'"
-                                + "}")), 0);
+                                + "}")));
                     } catch (JSONException ex) {
                         Logger.getLogger(ScriptengineLua.class.getName()).log(Level.SEVERE, null, ex);
                     }
@@ -300,11 +305,11 @@ public class ScriptengineLua extends OobdScriptengine {
                     //BaseLib.luaAssert(nArguments >0, "not enough args");
                     initRPC(callFrame, nArguments);
                     try {
-                        myself.getMsgPort().sendAndWait(new Message(myself, BusMailboxName, new Onion(""
+                        core.transferMsg(new Message(myself, BusMailboxName, new Onion(""
                                 + "{'type':'" + CM_BUSTEST + "',"
                                 + "'owner':"
-                                + "{'name':'" + myself.id + "'},"
-                                + "'command':'serFlush'}")), 0);
+                                + "{'name':'" + myself.getId() + "'},"
+                                + "'command':'serFlush'}")));
                     } catch (JSONException ex) {
                         Logger.getLogger(ScriptengineLua.class.getName()).log(Level.SEVERE, null, ex);
                     }
@@ -324,13 +329,13 @@ public class ScriptengineLua extends OobdScriptengine {
                     //BaseLib.luaAssert(nArguments >0, "not enough args");
                     initRPC(callFrame, nArguments);
                     try {
-                        myself.getMsgPort().sendAndWait(new Message(myself, BusMailboxName, new Onion("{"
+                        core.transferMsg(new Message(myself, BusMailboxName, new Onion("{"
                                 + "'type':'" + CM_BUSTEST + "',"
                                 + "'owner':"
-                                + "{'name':'" + myself.id + "'},"
+                                + "{'name':'" + myself.getId() + "'},"
                                 + "'command':'serDisplayWrite',"
                                 + "'data':'" + new BASE64Encoder().encode(getString(0).getBytes()) + "'"
-                                + "}")), 0);
+                                + "}")));
                     } catch (JSONException ex) {
                         Logger.getLogger(ScriptengineLua.class.getName()).log(Level.SEVERE, null, ex);
                     }
@@ -342,11 +347,24 @@ public class ScriptengineLua extends OobdScriptengine {
                 }
             });
 
+        File dir1 = new File(".");
+        try {
+            System.out.println("ScriptengineLua Current dir : " + dir1.getCanonicalPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream("enginelua.props"));
+        } catch (IOException ignored) {
+            Debug.msg("scriptengineterminal", DEBUG_WARNING, "couldn't load properties");
+        }
          
         try {
             //doScript("/OOBD.lbc");
-            doScript("OOBD.lbc");
+             System.out.println("Lua file" + props.getProperty("LuaDefaultScript", "OOBD.lbc"));
+            doScript(props.getProperty("LuaDefaultScript", "OOBD.lbc"));
         } catch (IOException ex) {
             Logger.getLogger(ScriptengineLua.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -356,9 +374,9 @@ public class ScriptengineLua extends OobdScriptengine {
             Message msg = getMsg(true);
             Onion on = msg.getContent();
             String vis = on.getOnionString("vis");
-            Debug.msg("scriptengineterminal", DEBUG_BORING, "Msg received:" + msg.getContent().toString());
+            Debug.msg("ScriptengineLua", DEBUG_BORING, "Msg received:" + msg.getContent().toString());
             try {
-
+                if (CM_UPDATE.equals(on.get("type"))){
                 core.transferMsg(new Message(this, CoreMailboxName, new Onion(""
                         + "{'type':'" + CM_VALUE + "',"
                         + "'owner':"
@@ -368,11 +386,12 @@ public class ScriptengineLua extends OobdScriptengine {
                         //+ "'value':'" + Integer.toString(i)
                         + "'value':'" + callFunction(vis, new Object[]{on.getOnionString("vis"), on.getOnionString("optid")})
                         + "'}")));
+                }
             } catch (JSONException ex) {
                 Logger.getLogger(ScriptengineTerminal.class.getName()).log(Level.SEVERE, null, ex);
             }
             i++;
-            Debug.msg("scriptengineterminal", DEBUG_BORING, "waked up after received msg...");
+            Debug.msg("ScriptengineLua", DEBUG_BORING, "waked up after received msg...");
 
         }
     }
@@ -386,6 +405,7 @@ public class ScriptengineLua extends OobdScriptengine {
     }
 
     public String callFunction(String functionName, Object[] params) {
+        System.out.println("function to call in Lua:" +functionName);
         LuaClosure fObject = (LuaClosure) state.getEnvironment().rawget(functionName);
         System.err.println(fObject.getClass().toString());
         System.err.println(fObject.toString());
