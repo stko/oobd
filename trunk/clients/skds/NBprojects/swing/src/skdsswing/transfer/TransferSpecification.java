9c7eb4ba4dcae404cbcf4d77e94126601ee4fc78
hamillton <hamillton@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2011-04-01 20:43:59 +0000

    MF-Y Update Information for YModem protocol. A Class is written for Calculation CRC16(CRC.java) and another Class is definition for Protocol constance (TransferSpecification.java) This are available in the Package "transfer"

---
+++ trunk/clients/skds/NBprojects/swing/src/skdsswing/transfer/TransferSpecification.java
@@ -0,0 +1,22 @@
+/*
+ * Define the YModem protocol Constant
+ * 
+ */
+
+package skdsswing.transfer;
+
+/**
+ *
+ * @author OOBD.org
+ */
+public final class TransferSpecification {
+    public static final byte SOH=0x01;
+    public static final byte STX=0x02;
+    public static final byte EOT=0x04;
+    public static final byte ACK=0x06;
+    public static final byte NAK=0x15;
+    public static final byte CAN=0x18;
+    public static final byte G=0x47;
+    public static final byte C=0x43;
+
+}
