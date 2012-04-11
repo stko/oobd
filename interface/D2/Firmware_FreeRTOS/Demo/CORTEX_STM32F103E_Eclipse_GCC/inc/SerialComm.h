20193f359925dd1d31dd14e422a8b4ed5cb5a152
stko <stko@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2012-04-11 12:59:43 +0000

    step 3/123

--- interface/D2/Firmware_FreeRTOS/Demo/CORTEX_STM32F103E_Eclipse_GCC/inc/SerialComm.h
+++
@@ -1,24 +0,0 @@
-/*
- * SerialTask.h
- *
- *  Created on: 20.06.2009
- *      Author: Dirki
- */
-
-#ifndef SERIALTASK_H_
-#define SERIALTASK_H_
-
-#define SERIAL_COMM_TASK_PRIORITY			( tskIDLE_PRIORITY + 3 )
- void InitSerialComm();
-void SerialSendStr(char const *str);
-void uart1_puts(char const *);
-void uart1_putc(char);
-int uart1_getc();
-void itoa(int value, char *str, int base);
-
-/*
-void uint8ToHex(char *buf, uint8_t value);
-void uint16ToHex(char *buf, uint16_t value);
-void uint32ToHex(char *buf, uint32_t value);
-*/
-#endif				/* SERIALTASK_H_ */
