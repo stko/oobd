c25ee8b28a3e28b285293c74c286563678aadc0e
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2013-02-25 10:54:31 +0000

    interim commit to move files from one machine to another

--- trunk/tools/bin/gpg/accessmanager.sh
+++ trunk/tools/bin/gpg/accessmanager.sh
@@ -2,17 +2,15 @@
 . settings.inc
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPTNAME=$( basename ${BASH_SOURCE[0]})
-echo $DIR
-echo $APPPASSPHRASEFILE
 
 # if no command line arg given
-# set rental to Unknown
+# set command to Unknown
 if [ -z $1 ]
 then
   command="no command"
 elif [ -n $1 ]
 then
-# otherwise make first arg as a rental
+# otherwise make first arg as a command
   command=$1
 fi
  
@@ -38,6 +36,10 @@ case $command in
 		echo "generate groupkeys from " $GROUPLIST 
 		$DIR/makegroups.sh $APPPASSPHRASEFILE  $GROUPLIST $GROUPPUBKEYS $GROUPSECKEYS
 	;;
+	"sendkeys")
+		echo "send new generated keys.." 
+		$DIR/sendgroupmails.vbs 
+	;;
 	"clean")
 		echo "remove the temporary files"
 		# rm -v $NEWUSERACCESS.sig_* $NEWUSERACCESS_* $TEMPUSERACCESS
@@ -62,6 +64,10 @@ case $command in
 		echo
 		echo $SCRIPTNAME " groupkeys"
 		echo "generate group keys out of the grouplist file"
+		echo
+		echo $SCRIPTNAME " sendkeys"
+		echo "send actual generated group files to users. Caution: Works only in Windows"
+		echo "in an Outlook/VBS environment"
 		echo
 		echo $SCRIPTNAME " clean"
 		echo "remove the temporary files"
