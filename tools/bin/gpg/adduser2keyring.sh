eb7bb1718faf029c44a2595078622e9321547344
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2013-03-02 12:54:23 +0000

    sucessful trial run

--- tools/bin/gpg/adduser2keyring.sh
+++ tools/bin/gpg/adduser2keyring.sh
@@ -1,10 +1,10 @@
 #!/bin/sh 
-export user=$(gpg --no-default-keyring --keyring ./userkey.pub --list-packets $1 | grep  -o "[A-Za-z0-0._]*@[A-Za-z0-9._]*")
+export user=$(gpg --no-default-keyring --keyring ./userkeyring.pub --list-packets $1 | grep  -o "[A-Za-z0-0._]*@[A-Za-z0-9._]*")
 echo $user
-gpg --yes --batch --no-default-keyring --keyring ./userkey.pub --delete-key $user
-gpg --no-default-keyring --keyring ./userkey.pub --import $1
-gpg --no-default-keyring --keyring ./userkey_archive.pub --import $1
+gpg --yes --batch --no-default-keyring --keyring ./userkeyring.pub --delete-key $user
+gpg --no-default-keyring --keyring ./userkeyring.pub --import $1
+gpg --no-default-keyring --keyring ./userkeyring_archive.pub --import $1
 echo List of actual keys
-gpg --no-default-keyring --keyring ./userkey.pub --list-keys
+gpg --no-default-keyring --keyring ./userkeyring.pub --list-keys
 echo List of archive keys
-gpg --no-default-keyring --keyring ./userkey_archive.pub --list-keys
+gpg --no-default-keyring --keyring ./userkeyring_archive.pub --list-keys
