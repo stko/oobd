e1694a164d3136bc410fc7469b5c785f72c2c8ab
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2013-05-13 10:39:05 +0000

    user- grouplist gets now also created typp correct

--- tools/bin/gpg/adduser2keyring.sh
+++ tools/bin/gpg/adduser2keyring.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-export user=$(gpg --no-default-keyring --keyring $1 --list-keys | grep  -o "[A-Za-z0-0._]*@[A-Za-z0-9._]*")
+export user=$(gpg --no-default-keyring --keyring $1 --list-keys | grep  -o "[A-Za-z0-9._]*@[A-Za-z0-9._]*")
 echo $user
 gpg --yes --batch --no-default-keyring --keyring ./userkeyring.pub --delete-key $user
 gpg --no-default-keyring --keyring ./userkeyring.pub --import $1
