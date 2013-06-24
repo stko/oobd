29db2bf7f63e50fe6efea01a0783fa851903e3d6
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2013-06-24 14:57:43 +0000

    making the knoppix live cd working from inside corperate networks by adding proxy handling

--- tools/dongleTest/setup.sh
+++ tools/dongleTest/setup.sh
@@ -1,10 +1,28 @@
 #!/bin/sh 
+
+test -f oobd.conf && source oobd.conf
+if test -n $HTTP_HOST -a -n $HTTP_PORT ; then
+	export HTTP_PROXY=$HTTP_HOST:$HTTP_PORT
+
+fi
+
+echo "Acquire::http::Proxy \"http://"$HTTP_PROXY"\"/;" > /tmp/pp
+sudo cp /tmp/pp /etc/apt/apt.conf.d/10-proxy
+
 sudo apt-get update
-sudo apt-get install subversion
-sudo apt-get install python-easygui
-sudo apt-get install python-bluetooth
-sudo apt-get install ussp-push
-sudo apt-get install blueman
+sudo apt-get install subversion python-easygui python-bluetooth ussp-push blueman
+
+# setting the subversion proxy
+
+if test -n $HTTP_HOST -a -n $HTTP_PORT ; then
+	echo "[global]" > ~/.subversion/servers
+	echo "http-proxy-host = "$HTTP_HOST >> ~/.subversion/servers
+	echo "http-proxy-port = "$HTTP_PORT >> ~/.subversion/servers
+
+fi
+
+
+
 mkdir -p ~/.config/autostart
 mkdir ~/bin
 rm -rf ~/bin/dongleTest
