4c58eedc5c918d945971c330bee5c2bb2fbcdee3
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2013-07-17 16:51:31 +0000

    scripts now run quite smoothly, BT flashing and hardware test routines more or less complete, just USB flashing implemented, but not tested yet

--- trunk/tools/dongleTest/downloadBins.sh
+++ trunk/tools/dongleTest/downloadBins.sh
@@ -9,5 +9,18 @@ fi
 echo $HTTP_PROXY
 mkdir flash
 # for some reason we need to use http instead the original https URL
-wget --no-cache --cache=off http://oobd.googlecode.com/files/Flashloader_Package.zip -O Flashloader_Package.zip	
-unzip -d flash Flashloader_Package.zip
+wget --no-cache --cache=off http://oobd.googlecode.com/files/Flashloader_Package.zip -O Flashloader_Package.zip.tmp	
+if [ $? -ne 0 ]; then
+	dialog --title "File update failed"  --yesno "Continue anyway?" 6 25
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+else
+	cp Flashloader_Package.zip.tmp Flashloader_Package.zip
+fi
+if [ ! -f Flashloader_Package.zip ]; then
+	dialog --title 'Error' --msgbox 'No flash files found\ Abort' 5 20
+	exit 1
+fi
+unzip -o -d flash Flashloader_Package.zip
+exit 0
