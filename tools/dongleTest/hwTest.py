df0ee625f7913aa9ce7af1b6894f76ea925990cd
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2013-09-15 04:59:48 +0000

    added the KL- test to the test run made the text output in colors for better readability

--- tools/dongleTest/hwTest.py
+++ tools/dongleTest/hwTest.py
@@ -7,6 +7,25 @@ import sys
 # to install easygui, use "sudo apt-get install python-easygui"
 from easygui import *
 
+class bcolors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+
+	def disable(self):
+		self.HEADER = ''
+		self.OKBLUE = ''
+		self.OKGREEN = ''
+		self.WARNING = ''
+		self.FAIL = ''
+		self.ENDC = ''
+
+
+
+
 testSet=[
 {'cmd':'p 0 0 0\r','res':r'.*(OBD).*','next':4,'err':1,'descr':'Looking for OOBD ID','okText':'OK: OOBD String received','errText':'Error: No correct Answer'},
 {'cmd':'p 0 0 0\r','res':r'.*(OBD).*','next':4,'err':2,'descr':'Looking for OOBD ID','okText':'OK: OOBD String received','errText':'Error: No correct Answer'},
@@ -26,29 +45,38 @@ testSet=[
 {'cmd':'p 1 2 3 1000\r','res':r'.*(.).*','next':15,'err':-1,'descr':'Buzzer on','okText':'OK: Buzzer turned on','errText':'Error: No correct Answer','dialogtext':'Do you hear the Buzzer?'},
 {'cmd':'p 1 2 3 0\r','res':r'.*(.).*','next':16,'err':-1,'descr':'Buzzer off','okText':'OK: Red LED turned off','errText':'Error: No correct Answer'},
 
+# KL-Line Test
+{'cmd':'p  1 2 4 0\r','res':r'.*(.).*','next':17,'err':-1,'descr':' K-Line TX OFF (Transmit mode) ','okText':'OK: K-Line TX OFF','errText':'Error: No correct Answer'},
+{'cmd':'p  1 2 5 1\r','res':r'.*(.).*','next':18,'err':-1,'descr':' L-Line ON - 0V ','okText':'OK: L-Line ON','errText':'Error: No correct Answer'},
+{'cmd':'p  0 0 11\r','res':r'.*(high).*','next':19,'err':-1,'descr':' read K-Line Level ','okText':'OK: K-Line HIGH','errText':'Error: No correct Answer'},
+{'cmd':'p  1 2 5 0\r','res':r'.*(.).*','next':20,'err':-1,'descr':' L-Line OFF - 12V (PullUp) ','okText':'OK: L-Line OFF','errText':'Error: No correct Answer'},
+{'cmd':'p  0 0 11\r','res':r'.*(low).*','next':21,'err':-1,'descr':' read K-Line Level ','okText':'OK: K-Line LOW','errText':'Error: No correct Answer'},
+
+
+
 # high speed test
-{'cmd':'p 8 2 0\r','res':r'.*(.).*','next':17,'err':-1,'descr':'deactivate Bus','okText':'OK: Bus deactivated','errText':'Error: No correct Answer'},
-{'cmd':'p 8 3 0\r','res':r'.*(.).*','next':18,'err':-1,'descr':'set CAN 500kb / 11b','okText':'OK: Bus set','errText':'Error: No correct Answer'},
-{'cmd':'p 8 4 0\r','res':r'.*(.).*','next':19,'err':-1,'descr':'set Channel 0','okText':'OK: Channel set','errText':'Error: No correct Answer'},
-{'cmd':'p 8 2 3\r','res':r'.*(.).*','next':20,'err':-1,'descr':'set Bus active','okText':'OK: Bus activated','errText':'Error: No correct Answer'},
-{'cmd':'p 6 5 $720\r','res':r'.*(.).*','next':21,'err':-1,'descr':'set Module ID 720 (Cluster)','okText':'OK: Module ID set','errText':'Error: No correct Answer'},
-{'cmd':'p 8 10 1 $07FF\r','res':r'.*(.).*','next':22,'err':-1,'descr':'set CAN Filter','okText':'OK: CAN Filter set','errText':'Error: No correct Answer'},
-{'cmd':'p 8 11 1 $0000\r','res':r'.*(.).*','next':23,'err':-1,'descr':'set CAN Mask ','okText':'OK: CAN Mask set','errText':'Error: No correct Answer'},
-{'cmd':'p 6 9 $728\r','res':r'.*(.).*','next':24,'err':-1,'descr':'set Send ID','okText':'OK: Send ID set','errText':'Error: No correct Answer'},
-{'cmd':'1002\r','res':r'^(50).*','next':27,'err':25,'descr':'send Tester present','okText':'OK: Answer from Cluster','errText':'Error: No correct Answer from Cluster'},
-{'cmd':'1002\r','res':r'^(50).*','next':27,'err':26,'descr':'send Tester present','okText':'OK: Answer from Cluster','errText':'Error: No correct Answer from Cluster'},
-{'cmd':'1002\r','res':r'^(50).*','next':27,'err':-1,'descr':'send Tester present','okText':'OK: Answer from Cluster','errText':'Error: No correct Answer from Cluster'},
+{'cmd':'p 8 2 0\r','res':r'.*(.).*','next':22,'err':-1,'descr':'deactivate Bus','okText':'OK: Bus deactivated','errText':'Error: No correct Answer'},
+{'cmd':'p 8 3 0\r','res':r'.*(.).*','next':23,'err':-1,'descr':'set CAN 500kb / 11b','okText':'OK: Bus set','errText':'Error: No correct Answer'},
+{'cmd':'p 8 4 0\r','res':r'.*(.).*','next':24,'err':-1,'descr':'set Channel 0','okText':'OK: Channel set','errText':'Error: No correct Answer'},
+{'cmd':'p 8 2 3\r','res':r'.*(.).*','next':25,'err':-1,'descr':'set Bus active','okText':'OK: Bus activated','errText':'Error: No correct Answer'},
+{'cmd':'p 6 5 $720\r','res':r'.*(.).*','next':26,'err':-1,'descr':'set Module ID 720 (Cluster)','okText':'OK: Module ID set','errText':'Error: No correct Answer'},
+{'cmd':'p 8 10 1 $07FF\r','res':r'.*(.).*','next':27,'err':-1,'descr':'set CAN Filter','okText':'OK: CAN Filter set','errText':'Error: No correct Answer'},
+{'cmd':'p 8 11 1 $0000\r','res':r'.*(.).*','next':28,'err':-1,'descr':'set CAN Mask ','okText':'OK: CAN Mask set','errText':'Error: No correct Answer'},
+{'cmd':'p 6 9 $728\r','res':r'.*(.).*','next':29,'err':-1,'descr':'set Send ID','okText':'OK: Send ID set','errText':'Error: No correct Answer'},
+{'cmd':'1002\r','res':r'^(50).*','next':32,'err':30,'descr':'send Tester present','okText':'OK: Answer from Cluster','errText':'Error: No correct Answer from Cluster'},
+{'cmd':'1002\r','res':r'^(50).*','next':32,'err':31,'descr':'send Tester present','okText':'OK: Answer from Cluster','errText':'Error: No correct Answer from Cluster'},
+{'cmd':'1002\r','res':r'^(50).*','next':32,'err':-1,'descr':'send Tester present','okText':'OK: Answer from Cluster','errText':'Error: No correct Answer from Cluster'},
 # mid speed test
-{'cmd':'p 8 2 0\r','res':r'.*(.).*','next':28,'err':-1,'descr':'deactivate Bus','okText':'OK: Bus deactivated','errText':'Error: No correct Answer'},
-{'cmd':'p 8 3 1\r','res':r'.*(.).*','next':29,'err':-1,'descr':'set CAN 125kb / 11b','okText':'OK: Bus set','errText':'Error: No correct Answer'},
-{'cmd':'p 8 4 1\r','res':r'.*(.).*','next':30,'err':-1,'descr':'set Channel 1','okText':'OK: Channel set','errText':'Error: No correct Answer'},
-{'cmd':'p 8 2 3\r','res':r'.*(.).*','next':31,'err':-1,'descr':'set Bus active','okText':'OK: Bus activated','errText':'Error: No correct Answer'},
-{'cmd':'p 6 5 $741\r','res':r'.*(.).*','next':32,'err':-1,'descr':'set Module ID','okText':'OK: Module ID set','errText':'Error: No correct Answer'},
-{'cmd':'p 8 10 1 $07FF\r','res':r'.*(.).*','next':33,'err':-1,'descr':'set CAN Filter','okText':'OK: CAN Filter set','errText':'Error: No correct Answer'},
-{'cmd':'p 8 11 1 $0000\r','res':r'.*(.).*','next':34,'err':-1,'descr':'set CAN Mask ','okText':'OK: CAN Mask set','errText':'Error: No correct Answer'},
-{'cmd':'p 6 9 $749\r','res':r'.*(.).*','next':35,'err':-1,'descr':'set Send ID','okText':'OK: Send ID set','errText':'Error: No correct Answer'},
-{'cmd':'1002\r','res':r'^(50).*','next':-1,'err':36,'descr':'send Tester present','okText':'OK: Answer from DCU','errText':'Error: No correct Answer from DCU'},
-{'cmd':'1002\r','res':r'^(50).*','next':-1,'err':37,'descr':'send Tester present','okText':'OK: Answer from DCU','errText':'Error: No correct Answer from DCU'},
+{'cmd':'p 8 2 0\r','res':r'.*(.).*','next':33,'err':-1,'descr':'deactivate Bus','okText':'OK: Bus deactivated','errText':'Error: No correct Answer'},
+{'cmd':'p 8 3 1\r','res':r'.*(.).*','next':34,'err':-1,'descr':'set CAN 125kb / 11b','okText':'OK: Bus set','errText':'Error: No correct Answer'},
+{'cmd':'p 8 4 1\r','res':r'.*(.).*','next':35,'err':-1,'descr':'set Channel 1','okText':'OK: Channel set','errText':'Error: No correct Answer'},
+{'cmd':'p 8 2 3\r','res':r'.*(.).*','next':36,'err':-1,'descr':'set Bus active','okText':'OK: Bus activated','errText':'Error: No correct Answer'},
+{'cmd':'p 6 5 $741\r','res':r'.*(.).*','next':37,'err':-1,'descr':'set Module ID','okText':'OK: Module ID set','errText':'Error: No correct Answer'},
+{'cmd':'p 8 10 1 $07FF\r','res':r'.*(.).*','next':38,'err':-1,'descr':'set CAN Filter','okText':'OK: CAN Filter set','errText':'Error: No correct Answer'},
+{'cmd':'p 8 11 1 $0000\r','res':r'.*(.).*','next':39,'err':-1,'descr':'set CAN Mask ','okText':'OK: CAN Mask set','errText':'Error: No correct Answer'},
+{'cmd':'p 6 9 $749\r','res':r'.*(.).*','next':30,'err':-1,'descr':'set Send ID','okText':'OK: Send ID set','errText':'Error: No correct Answer'},
+{'cmd':'1002\r','res':r'^(50).*','next':-1,'err':41,'descr':'send Tester present','okText':'OK: Answer from DCU','errText':'Error: No correct Answer from DCU'},
+{'cmd':'1002\r','res':r'^(50).*','next':-1,'err':42,'descr':'send Tester present','okText':'OK: Answer from DCU','errText':'Error: No correct Answer from DCU'},
 {'cmd':'1002\r','res':r'^(50).*','next':-1,'err':-1,'descr':'send Tester present','okText':'OK: Answer from DCU','errText':'Error: No correct Answer from DCU'},
 ]
 
@@ -71,25 +99,25 @@ def testCommand(thisTest):
 		else:
 			recvDone=True
 	if res!="":
-		print res
 		matchObj = re.match( thisTest['res'] , res, re.M|re.I)
+		print res.replace("\r","\r\n")
 
 		if matchObj:
 			if 'dialogtext' in thisTest:
 				if ccbox(thisTest['dialogtext'], "Please Confirm"):     # show a Continue/Cancel dialog
-					print thisTest['okText']
+					print  bcolors.OKGREEN + thisTest['okText'] + bcolors.ENDC
 					return thisTest['next']
 				else:
-					print thisTest['errText']
+					print bcolors.FAIL + thisTest['errText'] + bcolors.ENDC
 					return thisTest['err']
 			else:
-				print thisTest['okText']
+				print bcolors.OKGREEN + thisTest['okText'] + bcolors.ENDC
 				return thisTest['next']
 		else:
-			print thisTest['errText']
+			print  bcolors.FAIL + thisTest['errText'] + bcolors.ENDC
 			return thisTest['err']
 	else:
-		print "Empty read Buffer!"		
+		print  bcolors.FAIL + "Empty read Buffer!" + bcolors.ENDC	
 		return thisTest['err']
 
 def connect(macAdress):
@@ -117,7 +145,7 @@ try:
 	gaugeSocket.send('\r\r\r')
 	gaugeSocket.recv(1024)
 except bluetooth.btcommon.BluetoothError as error:
-	print "Caught inital BluetoothError: ", error
+	print bcolors.FAIL + "Caught inital BluetoothError: ", error , bcolors.ENDC
 state=0
 while(state!=-1):    
 	try:
@@ -125,7 +153,7 @@ while(state!=-1):
 		state=testCommand(thisTest)
 		#time.sleep(1)
 	except bluetooth.btcommon.BluetoothError as error:
-		print "Caught BluetoothError: ", error
+		print bcolors.FAIL + "Caught BluetoothError: ", error , bcolors.ENDC
 		time.sleep(5)
 		gaugeSocket = connect()
 
