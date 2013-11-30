1e9b97aaef5b4b643d088d838fc1ebbb32cd254e
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2013-11-30 00:24:40 +0000

    added --no-check-certificate to avoid certificate error when downloading file

--- trunk/tools/dongleTest/downloadBins.sh
+++ trunk/tools/dongleTest/downloadBins.sh
@@ -9,7 +9,7 @@ fi
 echo $HTTP_PROXY
 mkdir flash
 # for some reason we need to use http instead the original https URL
-wget --no-cache --cache=off https://oobd.googlecode.com/svn/trunk/interface/Designs/CORTEX/STM32F103C8_Eclipse_GCC/D2/flashfiles/AllinOne/Flashloader_Package.zip -O Flashloader_Package.zip.tmp	
+wget --no-cache --no-check-certificate --cache=off https://oobd.googlecode.com/svn/trunk/interface/Designs/CORTEX/STM32F103C8_Eclipse_GCC/D2/flashfiles/AllinOne/Flashloader_Package.zip -O Flashloader_Package.zip.tmp	
 if [ $? -ne 0 ]; then
 	dialog --title "File update failed"  --yesno "Continue anyway?" 6 25
 	if [ $? -ne 0 ] ; then
