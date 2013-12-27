0a2cebd0e992b60df73c0a1c32fd702204dd17dc
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2013-12-27 06:49:21 +0000

    how embarrassing... The long forgotten commit... Sorry

--- tools/lib_lua/lua_utils.lua
+++ tools/lib_lua/lua_utils.lua
@@ -23,16 +23,26 @@ end
 -- helper function to translate a table identifier into its table data and the related ID,
 -- returns ID,  data
 	function translateTableID(id)
-	info = Split(id, "_")
-	-- print ("split into: tablename "..info[1].." and id "..info[2])
-	data =_G[info[1]]
-	--print (data)
-	data= data[info[1].."_"..info[2]]
-	--[[
-	print (data)
-	for n ,v in pairs(data) do
-		print (n,v)
+	local info = Split(id, "_")
+	return info[3] , _G[info[1]][info[1].."_"..info[2].."_"..info[3]] --nice trick to get a variable just by name :-)
+end
+
+
+-- helper function to get a subtable just out of the ID. If wholeTable is true, then the function returns the whole subtable, if wholeTable is false, it only return that particual element out of the subtable which is indexed by the string
+-- returns ID,  data
+	function getSubTable(id, level,wholeTable)
+	local info = Split(id, "_")
+	local data =_G[info[1]]
+	if data ~=nil and #info>2 then
+		data= data[info[1].."_"..info[2].."_"..info[3]]
 	end
-	--]]
-	return info[2] , _G[info[1]][info[1].."_"..info[2]] --nice trick to get a variable just by name :-)
+	levCount = 0
+	while levCount < level and data ~=nil and (levCount *2 + 4 )< #info do
+		data = data[ info[ levCount * 2 + 4]]
+		if not(wholeTable and levCount == level -1) then -- if wholetable is set, then the last catch to get the last sudata. element need to be surpressed to return the whole subtable and not only one element out of it
+			data=data[ info[ levCount * 2 + 4] .. "_" ..info[ levCount * 2 + 5]]
+		end
+		levCount = levCount +1
+	end
+	return data
 end
