2a490522831ff313f3c97b6dcdcd469a8662a3eb
michael.luxen@gmail.com <michael.luxen@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2014-01-13 12:11:33 +0000

    1. bugfix for VIN handling Mode9, PID02 2. move nrcCodes to lua_utils.lua

--- trunk/lua-scripts/obdII-standard/OOBD.lua
+++ trunk/lua-scripts/obdII-standard/OOBD.lua
@@ -6,52 +6,7 @@ local BusID = "HS-CAN";
 local ModuleID = 0;  -- default functionl request ID for emission related ECUs
 
 dofile("../../tools/lib_lua/serial_dxm.lua")
-
----------------------- Negative response codes (NRC) --------------------------------------
--- see: http://www.autosar.org  => R4.0 AUTOSAR_SWS_DiagnosticCommunicationManager
-
-nrcCodes = {
-	[0x10] = 'General reject',
-	[0x11] = 'Service not supported (in active Diag session)',
-	[0x12] = 'Subfunction not supported (in active Diag session)',
-	[0x13] = 'Incorrect message length or invalid format',
-	[0x14] = 'Response to long',
-	[0x21] = 'Busy - Repeat request',
-	[0x22] = 'Conditions not correct',
-	[0x24] = 'Request sequence error',
-	[0x25] = 'No response from subnet component',
-	[0x26] = 'Failure prevents execution of requested action',
-	[0x31] = 'Request out of range',
-	[0x33] = 'Securtiy access denied',
-	[0x35] = 'Invalid key',
-	[0x36] = 'Exceed number of attempts',
-	[0x37] = 'Required time delay not expired',
-	[0x70] = 'Upload download not accepted',
-	[0x71] = 'Transfer data suspended',
-	[0x72] = 'General programming failure',
-	[0x73] = 'Wrong block sequence counter',
-	[0x78] = 'Reqeust correctly received - Response pending',
-	[0x7E] = 'Subfunction not supported in active session',
-	[0x7F] = 'System internal error',
-	[0x81] = 'RPM too high',
-	[0x82] = 'RPM too low',
-	[0x83] = 'Engine running',
-	[0x84] = 'Engine not running',
-	[0x85] = 'Engine runtime too low',
-	[0x86] = 'Temperature too high',
-	[0x87] = 'Temperature too low',
-	[0x88] = 'Vehicle speed too high',
-	[0x89] = 'Vehicle speed too low',
-	[0x8A] = 'Throttle pedal too high',
-	[0x8B] = 'Throttle pedal too low',
-	[0x8C] = 'Transmission range not in neutral',
-	[0x8D] = 'Transmission range not in gear',
-	[0x8F] = 'Brake not closed',
-	[0x90] = 'Sifter lever not in park',
-	[0x91] = 'Torque converter clutch locked',
-	[0x92] = 'Voltage too high',
-	[0x93] = 'Voltage too low',
-}
+dofile("../../tools/lib_lua/lua_utils.lua")
 
 ---------------------- Vehicle Info Menu --------------------------------------
 
@@ -59,7 +14,7 @@ function vin(oldvalue,id)
 	echoWrite("0902\r")
 	udsLen=receive()
 	if udsLen>0 then
-		if udsBuffer[1]==tonumber(9)+40 then
+		if udsBuffer[1]==0x49 then
 			local pos=4
 			local res=""
 			while pos <= udsLen and pos < 36 do
@@ -245,24 +200,6 @@ function createCall(availPIDs, id, title, func)
 		print("Id-String=",idstring,id)
 		addElement(title, func,"-",0x6, "0x"..string.format("%X",id))
 	end
-end
-
-function selectModuleID(oldvalue,id)
-	index = tonumber(oldvalue)
-
-	if index == 0 then
-		setModuleID("7DF") -- set legislated OBD/WWH-OBD functional request ID
-		setCANFilter(1,"7E8","7FF") -- set CAN-Filter of ECU request/response CAN-ID range
-		setSendID("7E8") 	-- set default to legislated OBD/WWH-OBD physical response ID (ECU #1 - ECM, Engince Control Module)
-		ModuleID = oldvalue;
-	else
-		setModuleID(string.format("%3X",index+0x7E0-1)) -- set legislated OBD/WWH-OBD functional request ID
-		setCANFilter(1,string.format("%3X",index+0x7E8-1),"7FF") -- set CAN-Filter of ECU request/response CAN-ID range
-		setSendID(string.format("%3X",index+0x7E8-1)) 	-- set default to legislated OBD/WWH-OBD physical response ID
-		ModuleID = oldvalue;
-	end
-
-	return oldvalue 
 end
 
 function createCMD01Menu(oldvalue,id)
