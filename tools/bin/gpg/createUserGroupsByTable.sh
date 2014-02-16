43baa5ce19dafbfd45edb3805ce705e0e1940792
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2014-02-16 05:09:53 +0000

    for better error traceability, outgoing mails contains now the fingerprints of the used keys

--- tools/bin/gpg/createUserGroupsByTable.sh
+++ tools/bin/gpg/createUserGroupsByTable.sh
@@ -11,14 +11,18 @@ while read line ; do
 	echo "user: [$user]"
 	echo "group:[$group]"
 	gpg --yes --batch --no-default-keyring --secret-keyring ./oobd_groups.sec  --export-secret-key $group >> $user.groupring.tmp
+	gpg --yes --batch --no-default-keyring --keyring ../oobd_groups.pub  --fingerprint $group >> $user.groupkeys.fingerprint
 done < $1
 for user in "${!USERS[@]}"
 do
 	echo "key :" $user
 	echo "value:" ${USERS[$user]}
+	echo "---- The fingerprints above are the groups for which you are actual authorized for.---------" >> $user.groupkeys.fingerprint
+	echo "---- The fingerprint below is your public key which the groupkeys are encrypted with ----" >> $user.groupkeys.fingerprint
 	gpg --yes --batch --no-default-keyring --secret-keyring ./$user.groupring.tmp  -o $user.groupring --export-secret-keys
-	echo "adninreader :" $ADMINREADER
+	echo "adminreader :" $ADMINREADER
 
+	gpg --yes --batch --no-default-keyring  --keyring ./userkeyring.pub --fingerprint $user >> $user.groupkeys.fingerprint
 	gpg --yes --batch --no-default-keyring --trust-model always  --keyring ./userkeyring.pub --recipient $user $ADMINREADER --output $user.groupkeys --encrypt $user.groupring
 #	gpg -v --yes --batch --no-default-keyring --trust-model always  --keyring ./userkeyring.pub --recipient $user --output $user.groupkeys --encrypt ./oobd_groups.sec
 	grep "$user" useraccess.txt > $user.groupkeys.lst
