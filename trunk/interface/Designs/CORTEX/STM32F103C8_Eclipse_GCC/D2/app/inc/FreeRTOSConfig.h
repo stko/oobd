d42252ccbb1af1bdacd26e84bc88cec73bedb7c5
michael.luxen@gmail.com <michael.luxen@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2014-06-03 11:21:51 +0000

    modify configTOTAL_HEAP_SIZE from 10240 bytes back to the old value 17x1024 bytes => now firmware is working again as before but with based on FreeRTOS v8.0 and heap4 memory management

--- trunk/interface/Designs/CORTEX/STM32F103C8_Eclipse_GCC/D2/app/inc/FreeRTOSConfig.h
+++ trunk/interface/Designs/CORTEX/STM32F103C8_Eclipse_GCC/D2/app/inc/FreeRTOSConfig.h
@@ -86,7 +86,7 @@
 #define configTICK_RATE_HZ                      1000
 #define configMAX_PRIORITIES                    5
 #define configMINIMAL_STACK_SIZE                128
-#define configTOTAL_HEAP_SIZE                   10240
+#define configTOTAL_HEAP_SIZE                   (( size_t ) ( 17 * 1024 ))
 #define configMAX_TASK_NAME_LEN                 16
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
