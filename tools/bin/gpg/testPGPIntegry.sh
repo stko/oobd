9181d59930e9af6cd496293fdf25860d758103ce
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2014-10-01 13:25:57 +0000

    Wrote a script to check consistency of the users key files combined with encrypted scripts

---
+++ tools/bin/gpg/testPGPIntegry.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+rm actgroupkeys.sec
+echo "When entering pathes, you can use the bash autocomplete tab key function"
+echo "use a leading ./ for key files in local directory, as gpg requires this"
+read -e -p "Enter the full path to your userkey.pub file: " PUBFILE
+PUBPATH=${PUBFILE%/*}
+read -e -p "Enter the full path to your userkey.sec file: " -i $PUBPATH  SECFILE 
+read -e -p "Enter the full path to your groupkey.sec file: " -i $PUBPATH  SECGROUPFILE 
+read -e -p "Enter the full path to the public oobd_groups.pub file: "  PUBGROUPFILE 
+PUBGROUPPATH=${PUBGROUPFILE%/*}
+read -e -p "Enter the full path to the application key data file: " -i $PUBGROUPPATH  appPassPhraseFile
+read -e -p "Enter the full path to a pgp Script file for testing: " -i $PUBPATH SCRIPTFILE
+gpg --no-default-keyring --keyring "$PUBFILE" --secret-keyring "$SECFILE" --output actgroupkeys.sec --decrypt "$SECGROUPFILE"
+if [ $? -eq 0 ] ; then
+	echo "--------------------------------"
+	echo "Step 1: the user keys allowed to decrypt the group keys"
+	gpg  -o - --decrypt $appPassPhraseFile | gpg --passphrase-fd 0 --batch --yes --no-default-keyring --keyring "$PUBGROUPFILE" --secret-keyring ./actgroupkeys.sec --list-packets "$SCRIPTFILE"
+	if [ $? -eq 0 ] ; then
+		echo "--------------------------------"
+		echo "Everything ok :-)"
+	else
+		echo "--------------------------------"
+		echo "ERROR: encrypted script file  does not fit to group keys"
+	fi
+
+else
+	echo "--------------------------------"
+	echo "ERROR groupkey.sec can't be decrypted with users key files"
+
+fi
+rm actgroupkeys.sec
--- tools/bin/gpg/testScriptIntegry.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo collect all necessary key files first in one directory and start this script from there, just with the script to evaluate as single parameter
-gpg --no-default-keyring --secret-keyring ./userkey.sec --keyring ./userkey.pub -o secret_groupring --decrypt ./groupkey.sec 
-gpg --no-default-keyring --secret-keyring ./secret_groupring --keyring ./oobd_groups.pub  $1
