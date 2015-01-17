cf47c67847149944c937e0aae8052f0487d047b5
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2015-01-17 05:35:44 +0000

    working on TCP connectivity for WiFi Dongles, just started

---
+++ trunk/clients/skds/base_src/org/oobd/base/port/ComPort_Telnet.java
@@ -0,0 +1,209 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.oobd.base.port;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Proxy;
+import java.net.Socket;
+import java.nio.channels.NotYetConnectedException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
+import org.oobd.base.Base64Coder;
+import org.oobd.base.bus.OobdBus;
+import org.oobd.base.support.Onion;
+
+//import gnu.io.*; // for rxtxSerial library
+
+/**
+ * 
+ * @author steffen
+ */
+public class ComPort_Telnet implements OOBDPort, Runnable {
+
+	OobdBus msgReceiver;
+	Integer port;
+	String Server;
+	Socket sock;
+	OutputStream out;
+	InputStream in;
+	static PortInfo[] udpAnouncements;
+
+	public ComPort_Telnet(String thisURL) {
+		String[] parts = thisURL.toString().split("://");
+		parts = parts[1].split(":");
+		Server = parts[0];
+		port = Integer.decode(parts[1]);
+		new Thread(this).start();
+	}
+
+	public boolean connect(Onion options, OobdBus receiveListener) {
+		msgReceiver = receiveListener;
+		attachShutDownHook();
+		try {
+			Socket sock = new Socket(Server, port);
+			out = sock.getOutputStream();
+			in = sock.getInputStream();
+			return true;
+		} catch (IOException ex) {
+			Logger.getLogger(ComPort_Telnet.class.getName()).log(Level.SEVERE,
+					null, ex);
+			return false;
+		}
+
+	}
+
+	public boolean available() {
+		return sock != null;
+	}
+
+	public String connectInfo() {
+		if (available()) {
+			return "Remote Connect to " + Server + ":" + port;
+		} else {
+			return null;
+		}
+	}
+
+	public static PortInfo[] getPorts() {
+		if (udpAnouncements == null) {
+			PortInfo[] DeviceSet = new PortInfo[1];
+			DeviceSet[0] = new PortInfo("", "No D");
+			return DeviceSet;
+		} else {
+			return udpAnouncements;
+		}
+
+	}
+
+	public static void setPorts(PortInfo[] newPorts) {
+		udpAnouncements = newPorts;
+	}
+
+	public void run() {
+		while (true) {
+
+			int n;
+
+			try {
+				if (sock != null && sock.getInputStream() != null) {
+					n = sock.getInputStream().available();
+					if (n > 0) {
+						byte[] buffer = new byte[n];
+
+						n = sock.getInputStream().read(buffer, 0, n);
+
+						String recString = new String(buffer);
+						recString = recString.substring(0, n);
+						msgReceiver.receiveString(recString);
+
+					} else {
+						try {
+							Thread.sleep(10);
+						} catch (InterruptedException ex) {
+							// the VM doesn't want us to sleep anymore,
+							// so get back to work
+							Logger.getLogger(ComPort_Telnet.class.getName())
+									.log(Level.WARNING, null, ex);
+
+						}
+					}
+				} else {
+					// as this thread runs in an unstopped endless loop, as
+					// long
+					// there's no serial port open, we need to slow him down
+					// here...
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						// the VM doesn't want us to sleep anymore,
+						// so get back to work
+					}
+				}
+
+			} catch (Exception ex) {
+				Logger.getLogger(ComPort_Telnet.class.getName()).log(
+						Level.WARNING, "Unexpected error: Close down socket",
+						ex);
+				close();
+			}
+		}
+	}
+
+	public void attachShutDownHook() {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+
+			@Override
+			public void run() {
+				System.err.println("Inside Add Shutdown Hook");
+				close();
+				System.err.println("Websocket closed");
+			}
+		});
+		System.err.println("Shut Down Hook Attached.");
+
+	}
+
+	public void onMessage(String message) {
+		try {
+			Onion myOnion = new Onion(message);
+			msgReceiver.receiveString(myOnion.getOnionBase64String("reply"));
+		} catch (JSONException ex) {
+			Logger.getLogger(ComPort_Telnet.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+	}
+
+	public synchronized void write(String s) {
+		try {
+			Logger.getLogger(ComPort_Telnet.class.getName()).log(Level.INFO,
+					"Telnet output:{0}", s);
+			out.write(new Onion("{'msg':'" + Base64Coder.encodeString(s)
+					+ "','channel': '" + port + "'}").toString().getBytes());
+
+			// outStream.flush();
+		} catch (JSONException ex) {
+			Logger.getLogger(ComPort_Telnet.class.getName()).log(Level.SEVERE,
+					null, ex);
+		} catch (IOException ex) {
+			Logger.getLogger(ComPort_Telnet.class.getName()).log(Level.SEVERE,
+					null, ex);
+		} catch (NotYetConnectedException ex) {
+			Logger.getLogger(ComPort_Telnet.class.getName()).log(Level.WARNING,
+					null, ex);
+		}
+	}
+
+	@Override
+	public void close() {
+		try {
+			if (in != null) {
+				in.close();
+				in = null;
+			}
+			if (out != null) {
+				out.close();
+				out = null;
+			}
+			if (sock != null) {
+				sock.close();
+				sock = null;
+			}
+		} catch (IOException ex) {
+
+		}
+	}
+
+	public int adjustTimeOut(int originalTimeout) {
+		// as the ws- based time could be much longer as a direct connection, we
+		// multiply the normal time
+		return originalTimeout * 1;
+	}
+}
