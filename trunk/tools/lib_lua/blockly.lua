8ba6d0283957d70623c802f8c19976d28b09e354
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2015-02-08 12:52:51 +0000

    Blockly: the invention part is done, now comes the normal testing & debugging stuff :-)

--- trunk/tools/lib_lua/blockly.lua
+++ trunk/tools/lib_lua/blockly.lua
@@ -9,7 +9,7 @@ library to wrap the blockly function calls to the existing lua libraries
 
 
 dofile("serial_dxm.lua")
-dofile("lib_lua.lua")
+dofile("lua_utils.lua")
 dofile("lib_uds.lua")
 
 
@@ -21,13 +21,13 @@ sets the bus channel, speed and active mode
 --]]
 
 function setDongleMode(busMode , busID , protocol)
+	echoWrite("p 1 "..protocol.."\r") -- activate Diagnostic protocol
 	setBus(busID)
 	if hardwareID == 2 then
 		echoWrite("p 5 "..busMode.."\r")
 	elseif hardwareID==3 or hardwareID==4 then
-		echoWrite("p 8 2 "..busMode.."\r")
+		echoWrite("p 8 2 "..busMode.." 0 \r")
 	end
-	echoWrite("p 1 "..protocol.."\r") -- activate Diagnostic protocol
 end
 
 
@@ -54,23 +54,24 @@ tempParamTable={}
 
 function evalResult(valService, valParams, valType , valStartBit , valBitLen , valOffset , valMult , valUnit)
   
+  result="unknown call.."
   if valType == "ascii" then
-    tempParamTable{'tempParamTable_NOP_'..valParams}={ sev_r = valService,  t="dummy title", call = "readAscPid", sd = {
-    sd_00 = {Bpos = valStartBit , Blen =  valBitLen , mult = valMult , offset = valOffset, unit = " "..valUnit, , dtype = "UNSIGNED", t="dummy subtitle"}}}
+    tempParamTable['tempParamTable_NOP_'..valParams]={ sev_r = valService,  t="dummy title", call = "readAscPid", sd = {
+    sd_00 = {Bpos = valStartBit , Blen =  valBitLen , mult = valMult , offset = valOffset, unit = " "..valUnit,  dtype = "UNSIGNED", t="dummy subtitle"}}}
     result=readAscPid("",'tempParamTable_NOP_'..valParams)
   end
   if valType == "bit" then
     bitvalue=Split(valUnit, "|")
-    tempParamTable{'tempParamTable_NOP_'..valParams}={ sev_r = valService, cmd = "id0x" .. valParams , t="dummy title", call = "readAscPid",  sd = {
+    tempParamTable['tempParamTable_NOP_'..valParams]={ sev_r = valService, cmd = "id0x" .. valParams , t="dummy title", call = "readBMPPid",  sd = {
 sd_00 = { by = math.floor(valStartBit / 8) , bi = valStartBit % 8, lt = bitvalue[1], ht = bitvalue[1]}}}
-},
     result=readBMPPid("",'tempParamTable_NOP_'..valParams)
   end
   if valType == "numeric" then
-    tempParamTable{'tempParamTable_NOP_'..valParams}={ sev_r = valService,  t="dummy title", call = "readNumPid", sd = {
-    sd_00 = {Bpos = valStartBit , Blen =  valBitLen , mult = valMult , offset = valOffset, unit = " "..valUnit, , dtype = "UNSIGNED", t="dummy subtitle"}}}
+    tempParamTable['tempParamTable_NOP_'..valParams]={ sev_r = valService,  t="dummy title", call = "readNumPid", sd = {
+    sd_00 = {Bpos = valStartBit , Blen =  valBitLen , mult = valMult , offset = valOffset, unit = " "..valUnit,  dtype = "UNSIGNED", t="dummy subtitle"}}}
     result=readNumPid("",'tempParamTable_NOP_'..valParams)
   end
+  return result
 end
 
 
@@ -89,29 +90,3 @@ function Start(oldvalue,id)
 end
 
 -------------------------------------------------------------------------------------------------------------------------------
-openPage('')
-  addElement("Description of this Value" , '', "-", 0x00, )
-pageDone()
-
-function CallName(oldvalue,id)
-local result = oldvalue
-  setDongleMode("off" , "HS-CAN", "1")
-  setModuleParams("7E0" , 50)
-  echoWrite(''..''.."\r")
-  local udsLen=receive()
-  if udsLen>0 then
-  if udsBuffer[1]==tonumber('',16) + 0x40  then
-    result = result=evalResult("ascii" , tonumber('') , tonumber('') , tonumber('') , tonumber('') , '')
-
-  elseif udsBuffer[1]== 0x7F then
-  return string.format(getLocalePrintf("nrc",string.format("0x%x",udsBuffer[3]), "NRC: 0x%02X"),udsBuffer[3])
-  else
-  return "Error"
-  end
-  else
-  return "NO DATA"
-  end
-return result
-end
-
-
