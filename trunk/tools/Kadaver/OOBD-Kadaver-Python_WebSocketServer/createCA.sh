d5180efdf58f3f02a96fb8bd42081bfe4058caf9
steffen.koehlers.de@gmail.com <steffen.koehlers.de@gmail.com@f834055d-ee70-0692-e1cb-8b28bc4b2f15>
2015-02-19 19:20:16 +0000

    finally found a way to create a certificate that works with the python server

---
+++ trunk/tools/Kadaver/OOBD-Kadaver-Python_WebSocketServer/createCA.sh
@@ -0,0 +1,6 @@
+
+#http://www.linux.org/threads/creating-a-self-signed-certificate-with-python.4591/
+python createCA.py
+cat oobd.luxen.de.* > luxen.cert
+echo call the python server then with luxen.cert as keyfile
+
