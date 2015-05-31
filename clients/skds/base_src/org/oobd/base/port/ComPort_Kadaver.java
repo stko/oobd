13d33cd1280a63c840dfa295208ebb51c8c2c759
Steffen Köhler <steffen@koehlers.de>
2015-05-31 07:53:53 +0100

    -  Kadaver-Websocket class now commonized with the Android version, which uses the simplified process of download the certificate directly from the server first to include in the app - getCertFromServer script simplified, which downloads the server certificate to be used in the app - svd_KadaverControl script finalized to control automatically the function of the server (see http://www.oobd.org/doku.php?id=doc:tools_kadavercontrol for details)

--- clients/skds/base_src/org/oobd/base/port/ComPort_Kadaver.java
+++ clients/skds/base_src/org/oobd/base/port/ComPort_Kadaver.java
@@ -40,284 +40,250 @@ import org.java_websocket.WebSocketImpl;
 import org.oobd.base.bus.OobdBus;
 
 /**
- * 
+ *
  * @author steffen
- * 
- *         https://github.com/TooTallNate/Java-WebSocket
+ *
+ * https://github.com/TooTallNate/Java-WebSocket
  */
 public class ComPort_Kadaver extends WebSocketClient implements OOBDPort {
 
-	OobdBus msgReceiver;
-	String channel;
-	URI wsURI;
-	String Server;
-	String protocol;
-	String proxyHost;
-	int proxyPort;
-	Proxy proxy;
+    OobdBus msgReceiver;
+    String channel;
+    URI wsURI;
+    String Server;
+    String protocol;
+    String proxyHost;
+    int proxyPort;
+    Proxy proxy;
 
-	public ComPort_Kadaver(java.net.URI wsURL, Proxy proxy, String proxyHost,
-			int proxyPort) {
-		super(wsURL);
-		if (proxy != Proxy.NO_PROXY) {
-			System.out.println("use proxy..");
-		}
-		this.setProxy(proxy);
-		this.proxy = proxy;
-		this.wsURI = wsURL;
-		this.proxyHost = proxyHost;
-		this.proxyPort = proxyPort;
-		String[] parts = wsURL.toString().split("@");
-		Server = wsURL.toString();
-		parts = parts[0].split("://");
-		protocol = parts[0];
-		channel = parts[1];
-	}
+    public ComPort_Kadaver(java.net.URI wsURL, Proxy proxy, String proxyHost,
+            int proxyPort) {
+        super(wsURL);
+        if (proxy != Proxy.NO_PROXY) {
+            System.out.println("use proxy..");
+        }
+        this.setProxy(proxy);
+        this.proxy = proxy;
+        this.wsURI = wsURL;
+        this.proxyHost = proxyHost;
+        this.proxyPort = proxyPort;
+        String[] parts = wsURL.toString().split("@");
+        Server = wsURL.toString();
+        parts = parts[0].split("://");
+        protocol = parts[0];
+        channel = parts[1];
+    }
 
-	public boolean connect(Onion options, OobdBus receiveListener) {
-		msgReceiver = receiveListener;
+    public boolean connect(Onion options, OobdBus receiveListener) {
+        msgReceiver = receiveListener;
 
-		WebSocketImpl.DEBUG = true;
-		if ("wss".equalsIgnoreCase(protocol)) {
-			// load up the key store
-			String STORETYPE = "JKS";
-			String KEYSTORE = "/org/oobd/base/port/keystore.jks";
-			// KEYSTORE = "/org/oobd/base/port/servercertCA.der";
-			String STOREPASSWORD = "ausderferne";
-			String KEYPASSWORD = "ausderferne";
-			String KEYMANAGERTYPE = "SunX509";
-			TrustManagerFactory tmf;
-			try {
-				if (System.getProperty("java.vm.name").equalsIgnoreCase(
-						"Dalvik")) {
-					STORETYPE = "BKS";
-					KEYSTORE = "/org/oobd/base/port/servercertCA.der";
-					KEYMANAGERTYPE = "X509";
+        WebSocketImpl.DEBUG = true;
+        if ("wss".equalsIgnoreCase(protocol)) {
+            // load up the key store
+            String KEYSTORE = "/org/oobd/base/port/servercertCA.der";
+            // KEYSTORE = "/org/oobd/base/port/servercertCA.der";
+             TrustManagerFactory tmf;
+            try {
 
-					// Android developer version starts here
-					// https://developer.android.com/training/articles/security-ssl.html
-
+	// covered from the Android developer version
+                // https://developer.android.com/training/articles/security-ssl.html
 					// Load CAs from an InputStream
-					// (could be from a resource or ByteArrayInputStream or ...)
-					CertificateFactory cf = CertificateFactory
-							.getInstance("X.509");
+                // (could be from a resource or ByteArrayInputStream or ...)
+                CertificateFactory cf = CertificateFactory
+                        .getInstance("X.509");
 
-					InputStream caInput = new BufferedInputStream(this
-							.getClass().getResourceAsStream(KEYSTORE));
-					Certificate ca;
-					try {
-						ca = cf.generateCertificate(caInput);
+                InputStream caInput = new BufferedInputStream(this
+                        .getClass().getResourceAsStream(KEYSTORE));
+                Certificate ca;
+                try {
+                    ca = cf.generateCertificate(caInput);
 						// System.out.println("ca=" + ((X509Certificate)
-						// ca).getSubjectDN());
-					} finally {
-						caInput.close();
-					}
+                    // ca).getSubjectDN());
+                } finally {
+                    caInput.close();
+                }
 
-					// Create a KeyStore containing our trusted CAs
-					String keyStoreType = KeyStore.getDefaultType();
-					KeyStore keyStore = KeyStore.getInstance(keyStoreType);
-					keyStore.load(null, null);
-					keyStore.setCertificateEntry("ca", ca);
+                // Create a KeyStore containing our trusted CAs
+                String keyStoreType = KeyStore.getDefaultType();
+                KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+                keyStore.load(null, null);
+                keyStore.setCertificateEntry("ca", ca);
 
-					// Create a TrustManager that trusts the CAs in our KeyStore
-					String tmfAlgorithm = TrustManagerFactory
-							.getDefaultAlgorithm();
-					tmf = TrustManagerFactory
-							.getInstance(tmfAlgorithm);
-					tmf.init(keyStore);
+                // Create a TrustManager that trusts the CAs in our KeyStore
+                String tmfAlgorithm = TrustManagerFactory
+                        .getDefaultAlgorithm();
+                tmf = TrustManagerFactory
+                        .getInstance(tmfAlgorithm);
+                tmf.init(keyStore);
 					// Android developer version ends here
 
-				} else {
-					// Java only vesion starts here
+                /*
 
-					KeyStore ks = KeyStore.getInstance(STORETYPE);
-					ks.load(this.getClass().getResourceAsStream(KEYSTORE),
-							STOREPASSWORD.toCharArray());
-					KeyManagerFactory kmf = KeyManagerFactory
-							.getInstance(KEYMANAGERTYPE);
-					kmf.init(ks, KEYPASSWORD.toCharArray());
-					tmf = TrustManagerFactory
-							.getInstance(KEYMANAGERTYPE);
-
-					tmf.init(ks);
-
-					// java only version ends here
-
-				}
-
-				/*
-
-   */
-
-				SSLContext sslContext = null;
-				sslContext = SSLContext.getInstance("TLS");
+                 */
+                SSLContext sslContext = null;
+                sslContext = SSLContext.getInstance("TLS");
 				// sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(),
-				// null); // the non android version---
-				sslContext.init(null, tmf.getTrustManagers(), null);
+                // null); // the non android version---
+                sslContext.init(null, tmf.getTrustManagers(), null);
 				// sslContext.init( null, null, null ); // will use java's
-				// default key and trust store which is sufficient unless you
-				// deal with self-signed certificates
+                // default key and trust store which is sufficient unless you
+                // deal with self-signed certificates
 
-				SSLSocketFactory factory = sslContext.getSocketFactory();// (SSLSocketFactory)
-																			// SSLSocketFactory.getDefault();
-				Socket s = new Socket(proxy); // as the websocket does not
-												// provide its socket, we have
-												// to overwrite his socket
-												// instead with our own one...
-				int port = uri.getPort();
-				if (port == -1) {
-					String scheme = uri.getScheme();
-					if (scheme.equals("wss")) {
-						port = WebSocket.DEFAULT_WSS_PORT;
-					} else if (scheme.equals("ws")) {
-						port = WebSocket.DEFAULT_PORT;
-					} else {
-						throw new RuntimeException("unkonow scheme" + scheme);
-					}
-				}
-				s.connect(new InetSocketAddress(wsURI.getHost(), port), 10000);
+                SSLSocketFactory factory = sslContext.getSocketFactory();// (SSLSocketFactory)
+                // SSLSocketFactory.getDefault();
+                Socket s = new Socket(proxy); // as the websocket does not
+                // provide its socket, we have
+                // to overwrite his socket
+                // instead with our own one...
+                int port = uri.getPort();
+                if (port == -1) {
+                    String scheme = uri.getScheme();
+                    if (scheme.equals("wss")) {
+                        port = WebSocket.DEFAULT_WSS_PORT;
+                    } else if (scheme.equals("ws")) {
+                        port = WebSocket.DEFAULT_PORT;
+                    } else {
+                        throw new RuntimeException("unkonow scheme" + scheme);
+                    }
+                }
+                s.connect(new InetSocketAddress(wsURI.getHost(), port), 10000);
 				// setSocket(s);
-				// connectBlocking(); // the socket needs to be connected before
-				// overlay it with SSL
-				// setSocket(factory.createSocket(s, wsURI.getHost(),
-				// wsURI.getPort(), true));
-				setSocket(factory.createSocket(s, wsURI.getHost(), port, true));
-				attachShutDownHook();
-				connectBlocking();
-				return true;
-			} catch (IOException ex) {
-				Logger.getLogger(ComPort_Kadaver.class.getName()).log(
-						Level.SEVERE, null, ex);
-				return false;
-			} catch (NoSuchAlgorithmException ex) {
-				Logger.getLogger(ComPort_Kadaver.class.getName()).log(
-						Level.SEVERE, null, ex);
-				return false;
-			} catch (CertificateException ex) {
-				Logger.getLogger(ComPort_Kadaver.class.getName()).log(
-						Level.SEVERE, null, ex);
-				return false;
-			} catch (KeyStoreException ex) {
-				Logger.getLogger(ComPort_Kadaver.class.getName()).log(
-						Level.SEVERE, null, ex);
-				return false;
+                // connectBlocking(); // the socket needs to be connected before
+                // overlay it with SSL
+                // setSocket(factory.createSocket(s, wsURI.getHost(),
+                // wsURI.getPort(), true));
+                setSocket(factory.createSocket(s, wsURI.getHost(), port, true));
+                attachShutDownHook();
+                connectBlocking();
+                return true;
+            } catch (IOException ex) {
+                Logger.getLogger(ComPort_Kadaver.class.getName()).log(
+                        Level.SEVERE, null, ex);
+                return false;
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(ComPort_Kadaver.class.getName()).log(
+                        Level.SEVERE, null, ex);
+                return false;
+            } catch (CertificateException ex) {
+                Logger.getLogger(ComPort_Kadaver.class.getName()).log(
+                        Level.SEVERE, null, ex);
+                return false;
+            } catch (KeyStoreException ex) {
+                Logger.getLogger(ComPort_Kadaver.class.getName()).log(
+                        Level.SEVERE, null, ex);
+                return false;
+            } catch (KeyManagementException ex) {
+                Logger.getLogger(ComPort_Kadaver.class.getName()).log(
+                        Level.SEVERE, null, ex);
+                return false;
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ComPort_Kadaver.class.getName()).log(
+                        Level.SEVERE, null, ex);
+                return false;
+            }
 
-			} catch (UnrecoverableKeyException ex) {
-				Logger.getLogger(ComPort_Kadaver.class.getName()).log(
-						Level.SEVERE, null, ex);
-				return false;
-			} catch (KeyManagementException ex) {
-				Logger.getLogger(ComPort_Kadaver.class.getName()).log(
-						Level.SEVERE, null, ex);
-				return false;
-			} catch (InterruptedException ex) {
-				Logger.getLogger(ComPort_Kadaver.class.getName()).log(
-						Level.SEVERE, null, ex);
-				return false;
-			}
+        } else {
+            attachShutDownHook();
+            try {
+                connectBlocking();
+                return true;
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ComPort_Kadaver.class.getName()).log(
+                        Level.SEVERE, null, ex);
+                return false;
+            }
+        }
 
-		} else {
-			attachShutDownHook();
-			try {
-				connectBlocking();
-				return true;
-			} catch (InterruptedException ex) {
-				Logger.getLogger(ComPort_Kadaver.class.getName()).log(
-						Level.SEVERE, null, ex);
-				return false;
-			}
-		}
+    }
 
-	}
+    public boolean available() {
+        return isOpen();
+    }
 
-	public boolean available() {
-		return isOpen();
-	}
+    public String connectInfo() {
+        if (isOpen()) {
+            return "Remote Connect to " + Server;
+        } else {
+            return null;
+        }
+    }
 
-	public String connectInfo() {
-		if (isOpen()) {
-			return "Remote Connect to " + Server;
-		} else {
-			return null;
-		}
-	}
+    public static PortInfo[] getPorts() {
 
-	public static PortInfo[] getPorts() {
+        PortInfo[] DeviceSet = new PortInfo[1];
+        DeviceSet[0] = new PortInfo("", "No Devices for Websockets");
+        return DeviceSet;
 
-		PortInfo[] DeviceSet = new PortInfo[1];
-		DeviceSet[0] = new PortInfo("", "No Devices for Websockets");
-		return DeviceSet;
+    }
 
-	}
+    public void attachShutDownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
 
-	public void attachShutDownHook() {
-		Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.err.println("Inside Add Shutdown Hook");
+                close();
+                System.err.println("Websocket closed");
+            }
+        });
+        System.err.println("Shut Down Hook Attached.");
 
-			@Override
-			public void run() {
-				System.err.println("Inside Add Shutdown Hook");
-				close();
-				System.err.println("Websocket closed");
-			}
-		});
-		System.err.println("Shut Down Hook Attached.");
+    }
 
-	}
+    public void onMessage(String message) {
+        try {
+            Onion myOnion = new Onion(message);
+            msgReceiver.receiveString(myOnion.getOnionBase64String("reply"));
+        } catch (JSONException ex) {
+            Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
+    }
 
-	public void onMessage(String message) {
-		try {
-			Onion myOnion = new Onion(message);
-			msgReceiver.receiveString(myOnion.getOnionBase64String("reply"));
-		} catch (JSONException ex) {
-			Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.SEVERE,
-					null, ex);
-		}
-	}
+    public synchronized void write(String s) {
+        try {
+            Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.INFO,
+                    "Serial output:{0}", s);
+            String onionMsg = new Onion("{'msg':'"
+                    + Base64Coder.encodeString(s) + "','channel': '" + channel
+                    + "'}").toString();
+            send(onionMsg);
 
-	public synchronized void write(String s) {
-		try {
-			Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.INFO,
-					"Serial output:{0}", s);
-			String onionMsg = new Onion("{'msg':'"
-					+ Base64Coder.encodeString(s) + "','channel': '" + channel
-					+ "'}").toString();
-			send(onionMsg);
+            // outStream.flush();
+        } catch (JSONException ex) {
+            Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } catch (NotYetConnectedException ex) {
+            Logger.getLogger(ComPort_Kadaver.class.getName()).log(
+                    Level.WARNING, null, ex);
+        } catch (org.java_websocket.exceptions.WebsocketNotConnectedException ex) {
+            Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
+    }
 
-			// outStream.flush();
-		} catch (JSONException ex) {
-			Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.SEVERE,
-					null, ex);
-		} catch (NotYetConnectedException ex) {
-			Logger.getLogger(ComPort_Kadaver.class.getName()).log(
-					Level.WARNING, null, ex);
-		} catch (org.java_websocket.exceptions.WebsocketNotConnectedException ex) {
-			Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.SEVERE,
-					null, ex);
-		}
-	}
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+    }
 
-	@Override
-	public void onOpen(ServerHandshake handshakedata) {
-	}
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+    }
 
-	@Override
-	public void onClose(int code, String reason, boolean remote) {
-	}
+    @Override
+    public void onError(Exception ex) {
+        Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.WARNING,
+                "Winsocket reports Error", ex);
+    }
 
-	@Override
-	public void onError(Exception ex) {
-		Logger.getLogger(ComPort_Kadaver.class.getName()).log(Level.WARNING,
-				"Winsocket reports Error", ex);
-	}
+    @Override
+    public void close() {
+        super.close();
+    }
 
-	@Override
-	public void close() {
-		super.close();
-	}
-
-	public int adjustTimeOut(int originalTimeout) {
+    public int adjustTimeOut(int originalTimeout) {
 		// as the ws- based time could be much longer as a direct connection, we
-		// multiply the normal time
-		return originalTimeout * 1;
-	}
+        // multiply the normal time
+        return originalTimeout * 1;
+    }
 }
--- tools/Kadaver/OOBD-Kadaver-Python_WebSocketServer/svd_KadaverControl.py
+++ tools/Kadaver/OOBD-Kadaver-Python_WebSocketServer/svd_KadaverControl.py
@@ -1,58 +1,87 @@
-import xmlrpclib
-#server = xmlrpclib.Server('http://localhost:9001/RPC2')
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
 from json import loads , dumps
 from base64 import encodestring, decodestring
 from websocket import create_connection
 from time import gmtime, strftime, sleep
 import sys
+import ssl
 import os
+from optparse import OptionParser
+
 
 lasterror=""
 result=""
-debug=False
 actStep=""
 loopCount=1
 myPID=str(os.getpid())
+textNoErr="all Ok"
+alright=False
+errorCount=0
 
-if len(sys.argv)!=2:
-	wsServerName="ws://localhost:9000/"
+parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
+parser.add_option("--url", default='ws://localhost:9000', type='string', action="store", dest="url", help="hostname (ws://localhost:9000)")
+parser.add_option("--cert", default='', type='string', action="store", dest="cert", help="cert file for SSL verification")
+parser.add_option("--loop", default=False, action="store_true", dest="loop", help="run in a loop instead only once")
+parser.add_option("--singleshot", default=False, action="store_true", dest="singleShot", help="runs until error condition is reached")
+parser.add_option("--debug", default=False, action="store_true", dest="debug", help="debug output")
+parser.add_option("--maxerror", default=3,  type='int', action="store", dest="errorCount", help="Nr of continious errors before set error flag")
+
+(options, args) = parser.parse_args()
+
+
+if options.cert!='':
+	
+	#context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+	#context.verify_mode = ssl.CERT_REQUIRED
+	#context.check_hostname = True
+	#context.load_verify_locations(sys.argv[2])
+	sslOptions=sslopt={
+		"ca_certs" : options.cert,
+		"ssl_version": ssl.PROTOCOL_TLSv1,
+		"check_hostname": False
+		}
 else:
-	wsServerName=sys.argv[1]
+	sslOptions=sslopt={"cert_reqs": ssl.CERT_NONE}
+	
 while(1):
 	try:
 		actStep="(1) init socket 1"
-		ws_dongle = create_connection(wsServerName, timeout=3)
+		ws_dongle = create_connection(options.url, timeout=3,sslopt=sslOptions)
 		actStep="(2) init socket 2"
-		ws_master = create_connection(wsServerName, timeout=3)
-		if debug:
-			print "init channel"
+		ws_master = create_connection(options.url, timeout=3,sslopt=sslOptions)
+		if options.debug:
+			print ("init channel")
 		actStep="(3) send dongle init"
 		ws_dongle.send(dumps({"reply": encodestring("init channel"), "channel" : encodestring(myPID) }))
-		if debug:
-			print "send master init"
+		if options.debug:
+			print ("send master init")
 		actStep="(4) send master init"
 		ws_master.send(dumps({"msg": encodestring("send master request"), "channel" : encodestring(myPID) }))
-		if debug:
-			print "Receiving at dongle side"
+		if options.debug:
+			print ("Receiving at dongle side")
 		actStep="(5) receive master init"
 		result =  ws_dongle.recv()
-		if debug:
-			print "Received as dongle:'%s'" % result
-			print "Answer from  dongle"
+		if options.debug:
+			print ("Received as dongle:'%s'" % result)
+			print ("Answer from  dongle")
 		actStep="(6) send dongle answer"
 		ws_dongle.send(dumps({"reply": encodestring("Answer from  dongle"), "channel" : encodestring(myPID) }))
-		if debug:
-			print "Receiving as master"
+		if options.debug:
+			print ("Receiving as master")
 		actStep="(7) receive dongle answer"
 		result =  ws_master.recv()
-		if debug:
-			print "Received as master:'%s'" % result
+		if options.debug:
+			print ("Received as master:'%s'" % result)
 		actStep="(8) close dongle socket"
 		ws_dongle.close()
 		actStep="(9) close master socket"
 		ws_master.close()
-		error="all Ok"
+		error=textNoErr
+		errorCount=0
 	except Exception as n:
+		errorCount+=1
 		error=  str(n) + " at step " + actStep
 		try:
 			ws_dongle
@@ -66,11 +95,22 @@ while(1):
 			ws_master = None
 		if ws_dongle != None:
 			ws_master.close()
-		if debug:
-			print "lastreceived:'%s' at step: %s" % (result,actStep)
-	if (lasterror!=error):
-		print strftime("%Y-%m-%d %H:%M:%S", gmtime()),"Loop:",loopCount, error
+	if (lasterror!=error or options.debug):
+		print ("%s\tLoop:\t%d\tErrorcount:\t%d\t%s" % (strftime("%Y-%m-%d %H:%M:%S", gmtime()),loopCount, errorCount, error))
+		if options.debug:
+			print ("lastreceived:'%s' at step: %s" % (result,actStep))
 	lasterror=error
+	alright = error==textNoErr
+	if (options.loop!=True): # in case, we don't want to run endless
+		if (alright or (not alright and errorCount> options.errorCount)): #it's either ok or the error level is rechead
+			if (not options.singleShot): # we dont want to wait until the error level is reached, so we quit
+				break
+			else: # otherways we need to check if we really have an error
+				if( not alright and errorCount> options.errorCount): #error reached?
+					break # quit the program
 	loopCount+=1
 	sleep(5)
-
+if (alright):
+	exit(0)
+else:
+	exit(1)
