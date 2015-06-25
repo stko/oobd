09127921849c3549500c3b6ac5e3f833d4bc0735
Steffen Köhler <steffen@koehlers.de>
2015-06-25 19:17:49 +0200

    echoLuaRev.sh now with git commit counter as revision reference

--- tools/lib_lua/echoLuaRev.sh
+++ tools/lib_lua/echoLuaRev.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 REV=$(svn info | grep Revision  |  awk -F: '{print $2}')
-if [ -z $REV ]; then
-REV=norev;
+if [ $? -ne 0 ] || [ -z $REV ]; then
+	REV=$(git rev-list HEAD --count)
+	if [ $? -ne 0 ] || [ -z $REV ]; then
+		REV=0;
+	fi
+else
+	REV=0;
 fi
 echo $1=\"$REV\"
