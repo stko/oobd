d48e963881286036ef9b01e181c1893b655b1f89
Steffen Köhler <steffen@koehlers.de>
2015-07-15 21:46:16 +0200

    some progress on the svg handling

--- webUI/libs/oobd/1/oobd.js
+++ webUI/libs/oobd/1/oobd.js
@@ -27,6 +27,34 @@
 })();
 
 
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+	Oobd.init('ws://localhost:8443');
+	registerOOBD("oobd_te-interface_serial:","");
+	registerOOBD("oobd_te-interface_version:","");
+	registerOOBD("oobd_te-interface_voltage:","");
+	registerOOBD("oobd_te-interface_bus:","");
+
+	// Get all svg images, which are defined as OOBDMap class
+	var a = document.getElementsByClassName("OOBDMap");
+	var oobdpatt = new RegExp("^oobd_"); // Regex pattern for svg area identification
+	for (index = 0; index < a.length; ++index) { // go through all images found
+		var svgDoc = a[index].contentDocument;
+		var svgItem = svgDoc.getElementsByTagName("*"); //list all image elements
+		for (i2 = 0; i2 < svgItem.length; ++i2) {
+			var name=svgItem[i2].getAttribute("id"); 
+			console.log("element:"+name);
+			 if (oobdpatt.test(name)){//is the element a oobd map area
+				console.log("passt: "+name);
+				Oobd.addObject(svgItem[i2],"");
+				svgItem[i2].setAttribute("style", "fill:#FF0000");
+			}
+		}
+	}
+
+    }
+}, 10);
 
 
 /**
@@ -60,7 +88,23 @@ if (typeof Oobd == "undefined") {
 						if (obj.type=="VALUE"){
 							console.log("b64_value: "+obj.value);
 							var server_message = atob(obj.value);
-							var owner = obj.to.name;
+							var owner = obj.to.name;	// Get all svg images, which are defined as OOBDMap class
+	var a = document.getElementsByClassName("OOBDMap");
+	var oobdpatt = new RegExp("^oobd_"); // Regex pattern for svg area identification
+	for (index = 0; index < a.length; ++index) { // go through all images found
+		var svgDoc = a[index].contentDocument;
+		var svgItem = svgDoc.getElementsByTagName("*"); //list all image elements
+		for (i2 = 0; i2 < svgItem.length; ++i2) {
+			var name=svgItem[i2].getAttribute("id"); 
+			console.log("element:"+name);
+			 if (oobdpatt.test(name)){//is the element a oobd map area
+				console.log("passt: "+name);
+				Oobd.addObject(svgItem[i2],"");
+				svgItem[i2].setAttribute("style", "fill:#FF0000");
+			}
+		}
+	}
+
 							console.log("owner:"+owner);
 							var h= document.getElementById(owner);
 							console.log("element:"+h.innerHTML);
@@ -76,17 +120,31 @@ if (typeof Oobd == "undefined") {
 				window.alert("Socket not supported");
 			}
 		},
-		add : function(id, initialValue) {
-				var h= document.getElementById(id);
-				h.oobd=new Object();
-				h.oobd.value=initialValue;
+		add : function(id, initialValue) { 
+				var obj= document.getElementById(id);
+				this.addObject(obj,initialValue);
+		},
+
+		splitID : function(id) { //splits the id into type and command
+				return id.match(/oobd_(\w+)-(.+)/); 
+		},
+
+		addObject : function(obj, initialValue) {
+				obj.oobd=new Object();
+				obj.oobd.value=initialValue;
 				thisElement = new Object();
-				thisElement["name"] = id;
-				thisElement["object"]=h;
+				thisElement["name"] = obj.getAttribute("id");
+				var params=this.splitID(obj.getAttribute("id"));
+				console.log(params); 
+				thisElement["type"] = params[1];
+				thisElement["command"] = params[2];
+				thisElement["object"]=obj;
+				obj.oobd.command = params[2];
 				this.visualizers.push(thisElement);
-				h.addEventListener("click", function(){
+				obj.addEventListener("click", function(){
 					console.log("clicked element "+this.id);
-					Oobd.connection.send("{'name'='"+this.id+"','opt'='','value'='"+btoa(this.oobd.value)+"','type'=1}");
+					console.log("clicked command "+obj.oobd.command);
+					Oobd.connection.send("{'name'='"+obj.oobd.command+"','opt'='','value'='"+btoa(this.oobd.value)+"','type'=1}");
 				});
 		}
 		
