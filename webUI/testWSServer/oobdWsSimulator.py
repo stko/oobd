0e980aff438c2d10b6d0b1111b56d2b7b06c6400
Steffen Köhler <steffen@koehlers.de>
2015-08-19 20:36:29 +0200

    timer and central update added

--- webUI/testWSServer/oobdWsSimulator.py
+++ webUI/testWSServer/oobdWsSimulator.py
@@ -66,7 +66,7 @@ class SimpleChat(WebSocket):
  
    def handleConnected(self):
       print self.address, 'connected'
-      self.sendMessage('{"type":"WRITESTRING" ,"value":"'+encodestring("Connected to OOBD").replace('\n', '')+'"}')
+      self.sendMessage('{"type":"WRITESTRING" ,"data":"'+encodestring("Connected to OOBD").replace('\n', '')+'"}')
       self.sendMessage('{"type":"VALUE" , "to":{"name":"gauge_speed:"}, "value":"'+encodestring("120").replace('\n', '')+'"}')
 
    def handleClose(self):
