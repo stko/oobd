ec91b5d67267c1104b3184d0477e69aa945508c1
Steffen Köhler <steffen@koehlers.de>
2015-12-05 11:03:00 +0100

    small correction on the RF test script, trial 2

--- testing/robotframework/local_settings_template.py
+++ testing/robotframework/local_settings_template.py
@@ -5,5 +5,7 @@ wsSocketTimeout = 30
 
 donglePort = "/tmp/DXM"
 
+
 # how to get a slack mesage url, see https://api.slack.com/incoming-webhooks
-slackURL = "https://hooks.slack.com/services/<yourpersonalChannelKey>"
+# encode the URL by echo -n "https://hooks.slack.com/services/<yourpersonalChannelKey>" | base64
+slackURL = "aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvYmxhYmx1Yg=="
