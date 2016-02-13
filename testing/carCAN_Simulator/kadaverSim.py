71d3795224a32f9c1fb8421e3cd2e727ff2c7b65
Steffen Köhler <steffen@koehlers.de>
2016-02-13 16:30:16 +0100

    kadaver endpoint written in python for testing

--- testing/carCAN_Simulator/kadaverSim.py
+++ testing/carCAN_Simulator/kadaverSim.py
@@ -1,45 +1,91 @@
 import os.path
 import websocket
-import sys
+import sys, getopt , thread
 import re
+import ssl
 import time
+import serial
 from json import loads, dumps
-from base64 import encodestring,decodestring, b64decode
+from base64 import encodestring,decodestring, b64decode, b64encode
 from pprint import pprint
 
-_wsSocket = None
+class kadaverSim(object):
 
-class webUIClient(object):
-
-	def __init__(self):
-		self._answer = ''
-
-	def open_webUI(self, wsURL, timeout):
-		global _wsSocket
+	def __init__(self,wsURL,connectID,serialPort):
+		self._wsURL=wsURL
+		self._connectID=connectID
+		self._serialPort=serialPort
 		sys.stderr.write("open "+wsURL+"\n")
-		_wsSocket = websocket.WebSocket()
-		if _wsSocket is None:
-			raise AssertionError("could not open webUI- Websocket!")
-		_wsSocket.connect(wsURL)
-		_wsSocket.settimeout(timeout)
 
-	def close_webUI(self):
-		global _wsSocket
-		if not _wsSocket is None:
-			sys.stderr.write("close webUI\n")
-			_wsSocket.close()
+		def on_error(ws, error):
+			print (error)
 
-	def send_webUI_command(self,cmd):
-		global _wsSocket
+		def on_close(ws):
+			print ("### closed ###")
+
+		def on_open(ws):
+			print ("### connected ###")
+			try:
+				self.send_kadaver_message("OOBD Rocks")
+				print ("### connected2 ###")
+			except Exception as n:
+				print "Exception: " , n
+				
+		def on_message(ws, message):
+			print (message)
+			try:
+				thisMsg=loads(message)
+				if "msg" in thisMsg:
+					self._serPortSocket.write(decodestring(thisMsg["msg"]).encode('utf-8'))
+				if "echo" in thisMsg and "msg" in thisMsg:
+					thisMsg["echo"]="client"
+					thisMsg["reply"]=""
+					thisMsg.pop("msg", None)
+					self._wsSocket.send(dumps(thisMsg))
+			except Exception as n:
+				print "Exception: " , n
+
+		self._wsSocket = websocket.WebSocketApp(wsURL,
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+		if self._wsSocket is None:
+			raise AssertionError("could not open kadaver- Websocket!")
+		self._serPortSocket = serial.Serial(serialPort, 38400, timeout=10)
+
+		def serialThread(*args):
+			while True:
+				# from pySerial 3.0 onwards: nrOfChars=self._serPortSocket.in_waiting
+				nrOfChars=self._serPortSocket.inWaiting()
+				if nrOfChars>0:
+					_input = self._serPortSocket.read(nrOfChars)
+					if len(_input) > 0:
+						print ('Got:', _input)
+						self.send_kadaver_message(_input)
+				else:
+					time.sleep(0.01)
+
+		thread.start_new_thread(serialThread, ())
+		self._wsSocket.on_open = on_open
+		# disable SSL cert check finally found on https://pypi.python.org/pypi/websocket-client/
+		# maybe I should just read the original documentaion more often first before googeling around ;-)
+		self._wsSocket.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+
+	def close_kadaver(self):
+		if not self._wsSocket is None:
+			sys.stderr.write("close kadaver\n")
+			self._wsSocket.close()
+
+	def send_kadaver_message(self,message):
+		thisMsg=loads('{ "reply" : "" , "channel" : "" }')
+		thisMsg["reply"]=b64encode(message)
+		thisMsg["channel"]=b64encode(self._connectID)
+		pprint (thisMsg)
 		self._flush()
-		try: # do a sanity check first, if the given string is really a well formed JSON string
-			loads(cmd)
-		except:
-			raise AssertionError("command is not a valid JSON string!: "+cmd+"\n")
-		if _wsSocket is None:
-			raise AssertionError("send_webUI_command: Websocket is not open!")
-		# sys.stderr.write("webUI cmd: "+cmd+"\n")
-		_wsSocket.send(cmd)      # write a string
+		if self._wsSocket is None:
+			raise AssertionError("send_kadaver_message: Websocket is not open!")
+		self._wsSocket.send(dumps(thisMsg))      # write a string
+		sys.stderr.write("send msg:"+dumps(thisMsg))
 
 	def answer_should_match(self, expected_answer):
 		self._answer = ""
@@ -60,8 +106,6 @@ class webUIClient(object):
 
 	def compareDicts(self,patternDict, inputDict):
 		for attr, value in patternDict.items():
-			#sys.stderr.write("Type of value:" +type(value).__name__+"\n");
-			#sys.stderr.write("Type of inputDict:" +type(inputDict[attr]).__name__+"\n");
 			if not attr in inputDict:
 				return False
 			elif type(value) != type(inputDict[attr]):
@@ -88,7 +132,6 @@ class webUIClient(object):
 							sys.stderr.write (" REGEX string compare for " + str(value) + "against " + str(inputValue) + " failed\n" )
 							return False
 					elif value != inputValue:
-						# sys.stderr.write("normal string test")
 						sys.stderr.write (" normal string compare for " + str(value) + " against " + str(inputValue) + " failed\n" )
 						return False
 
@@ -98,30 +141,51 @@ class webUIClient(object):
 		return True
 			
 	def _flush(self):
-		global _wsSocket
-		if _wsSocket is None:
-			raise AssertionError("_doLine: Websocket is not open!")
+		if self._wsSocket is None:
+			raise AssertionError("_flush: Websocket is not open!")
 		else:
-			timeout = _wsSocket.gettimeout()
-			_wsSocket.settimeout(0.3)
 			try:
 
 				s="dummy"
 				while s != "":
-					s= _wsSocket.recv()
+					s= self._wsSocket.recv()
 			except Exception as e:
 					pass
 					#raise AssertionError("flush error :-("+repr(e)+"\n")
-			_wsSocket.settimeout(timeout)
 
 	def _doLine(self):
-		global _wsSocket
-		if _wsSocket is None:
+		if self._wsSocket is None:
 			raise AssertionError("_doLine: Websocket is not open!")
 		else:
 			try:
-				s = _wsSocket.recv()          # read buffer
+				s = self._wsSocket.recv()          # read buffer
 				self._answer +=s
-				# sys.stderr.write("Answer "+self._answer+"\n") 
 			except Exception as e:
 				raise AssertionError("something went wrong :-("+repr(e)+"\n")
+			
+			
+def main(argv):
+	wsHost = 'wss://oobd.luxen.de/websockssl/'
+	connectID = '1234'
+	serialPort = '/tmp/DXM'
+	try:
+		opts, args = getopt.getopt(argv,"h:c:s:",["host=","connect-id=","serial-port="])
+	except getopt.GetoptError:
+		print ("kadaverSim.py -h <ServerURL> -c <connectID>")
+		print ("kadaverSim.py --host=<ServerURL> --connect-id=<connectID>")
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			wsHost = arg
+		elif opt in ("-c", "--connect-id"):
+			connectID = arg
+		elif opt in ("-s", "--serial-port"):
+			serialPort = arg
+	print('connectID  is "', connectID ,'"')
+	print('wsHost is "', wsHost ,'"')
+	print('serial Port is "', serialPort ,'"')
+	mySocket= kadaverSim( wsHost, connectID , serialPort)
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
