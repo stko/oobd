2ab6b69380e915721db2c4fb9127900f35b725c7
Steffen Köhler <steffen@koehlers.de>
2016-08-14 18:06:35 +0200

    preference json import now with import filter

--- clients/skds/base_src/org/oobd/base/Settings.java
+++ clients/skds/base_src/org/oobd/base/Settings.java
@@ -40,29 +40,49 @@ package org.oobd.base;
  }
  }
 
-Setzen eines Nullwertes in JSON:
-Try to set JSONObject.NULL instead of null:
+ Setzen eines Nullwertes in JSON:
+ Try to set JSONObject.NULL instead of null:
 
 
  */
-
-
+import java.util.Iterator;
+import static java.util.Spliterators.iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 import org.json.JSONException;
+import org.json.JSONObject;
 import static org.oobd.base.OOBDConstants.FT_PROPS;
 import org.oobd.base.support.Onion;
 import org.oobd.base.support.OnionNoEntryException;
 import org.oobd.base.support.OnionWrongTypeException;
 
-
 public class Settings {
-    
+
     static Onion prefs;
     static Settings myself;
     static Preferences pref;
+    static SavePreferenceJsonString savePrefsCallback;
+    static String prefsTemplateString = "{\n"
+            + "  \"Bluetooth_ServerProxyPort\": {\"type\" : \"int\" , \"description\" : \"(not used)\"},\n"
+            + "  \"Bluetooth_SerialPort\": {\"type\" : \"string\" , \"description\" : \"Serial Port\"},\n"
+            + "  \"Bluetooth_ConnectServerURL\": {\"type\" : \"string\" , \"description\" : \"\"},\n"
+            + "  \"Bluetooth_ServerProxyHost\": {\"type\" : \"string\" , \"description\" : \"\"},\n"
+            + "  \"UIHandler\": {\"type\" : \"string\" , \"description\" : \"\"},\n"
+            + "  \"ScriptDir\": {\"type\" : \"string\" , \"description\" : \"Script Directory\"},\n"
+            + "  \"Kadaver_ServerProxyPort\": {\"type\" : \"int\" , \"description\" : \"Proxy Port (0 to disable)\"},\n"
+            + "  \"Kadaver_SerialPort\": {\"type\" : \"string\" , \"description\" : \"Connect ID\"},\n"
+            + "  \"Kadaver_ConnectServerURL\": {\"type\" : \"string\" , \"description\" : \"Kadaver Server URL\"},\n"
+            + "  \"Kadaver_ServerProxyHost\": {\"type\" : \"string\" , \"description\" : \"Proxy Host\"},\n"
+            + "  \"Telnet_ServerProxyPort\": {\"type\" : \"int\" , \"description\" : \"(not used)\"},\n"
+            + "  \"Telnet_SerialPort\": {\"type\" : \"string\" , \"description\" : \"Server Host\"},\n"
+            + "  \"Telnet_ConnectServerURL\": {\"type\" : \"string\" , \"description\" : \"\"},\n"
+            + "  \"Telnet_ServerProxyHost\": {\"type\" : \"string\" , \"description\" : \"(not used)\"},\n"
+            + "  \"PGPEnabled\": {\"type\" : \"bool\" , \"description\" : \"(not used)\"},\n"
+            + "  \"ConnectType\": {\"type\" : \"string\" , \"description\" : \"Connection Type\"},\n"
+            + "  \"LibraryDir\": {\"type\" : \"string\" , \"description\" : \"HTML Library Directory\"},\n"
+            + "}";
 
     public class IllegalSettingsException extends Exception {
 
@@ -83,59 +103,91 @@ public class Settings {
         }
     }
 
-    
-    public interface getPreferenceJsonString {
+    public interface SavePreferenceJsonString {
 
-        String getprefString();
+        boolean getprefString(String prefs);
     }
 
-    public Settings(getPreferenceJsonString getPrefs) throws IllegalSettingsException {
-        try {
-            prefs= new Onion(getPrefs.getprefString());
-        } catch (JSONException ex) {
-             throw new IllegalSettingsException(ex);
-        }
-        myself=this;
+    public Settings(SavePreferenceJsonString savePrefs) throws IllegalSettingsException {
+        prefs = new Onion();
+        myself = this;
+        savePrefsCallback=savePrefs;
     }
-    
+
     public Settings(Preferences thisPrefs) throws IllegalSettingsException {
         try {
-            prefs=new Onion();
-            String[] sysKeys = thisPrefs.keys();
-            for (int i = 0; i < sysKeys.length; i++) { //copy system settings, if any exist
-                System.out.println(sysKeys[i] + ":" + thisPrefs.get(sysKeys[i], ""));
-                prefs.setValue(sysKeys[i].replaceAll("_", "/"), thisPrefs.get(sysKeys[i], ""));
+            String prefsString = thisPrefs.get("json", null);
+            if (prefsString != null) {
+                prefs = new Onion();
+                transferSettings(prefsString);
+                System.out.println("Prefs loaded from JSON String!");
+            } else {
+                prefs = new Onion();
+                String[] sysKeys = thisPrefs.keys();
+                for (int i = 0; i < sysKeys.length; i++) { //copy system settings, if any exist
+                    System.out.println(sysKeys[i] + ":" + thisPrefs.get(sysKeys[i], ""));
+                    prefs.setValue(sysKeys[i].replaceAll("_", "/"), thisPrefs.get(sysKeys[i], ""));
+                }
             }
             System.out.println(prefs);
-            myself=this;
-            pref=thisPrefs;
-        } catch (BackingStoreException ex) {
+            myself = this;
+            pref = thisPrefs;
+        } catch (JSONException|BackingStoreException ex) {
             Logger.getLogger(Settings.class.getName()).log(Level.SEVERE, null, ex);
             throw new IllegalSettingsException(ex);
         }
     }
-    
-    public static int getInt( String path, int defaultValue){
+
+    void transferSettings(String input) throws JSONException  {
+
+        Onion inputOnion = new Onion(input);
+        JSONObject templateJSON = new JSONObject(prefsTemplateString);
+
+        for (Iterator<String> iter = templateJSON.keys(); iter.hasNext();) {
+            String templateKey = iter.next();
+            String key = templateKey.replace("_", "/");
+            Object inputData;
+            try {
+                inputData = inputOnion.getOnionObject(key);
+                JSONObject template = templateJSON.getJSONObject(templateKey);
+                if (inputData != null) {
+                    switch (template.getString("type")) {
+                        case "int":
+                            prefs.put(key, (Integer) inputOnion.get(key));
+                            break;
+                        case "string":
+                            prefs.put(key, (String) inputOnion.get(key));
+                            break;
+                        case "boolean":
+                            prefs.put(key, (boolean) inputOnion.get(key));
+                            break;
+                    }
+                }
+            } catch (JSONException|OnionNoEntryException ex) {
+            }
+        }
+    }
+
+    public static int getInt(String path, int defaultValue) {
         //temporary tweak to make it work
-        path=path.replaceAll("_", "/");
+        path = path.replaceAll("_", "/");
         try {
             return myself.prefs.getOnionInt(path);
-        } catch (ClassCastException|OnionWrongTypeException | OnionNoEntryException ex) {
+        } catch (ClassCastException | OnionWrongTypeException | OnionNoEntryException ex) {
             myself.prefs.setValue(path, defaultValue);
             return defaultValue;
         }
     }
-    
-    
-    public static void setInt( String path, int value){
+
+    public static void setInt(String path, int value) {
         //temporary tweak to make it work
-        path=path.replaceAll("_", "/");
+        path = path.replaceAll("_", "/");
         myself.prefs.setValue(path, value);
     }
-  
-    public static String getString( String path, String defaultValue){
+
+    public static String getString(String path, String defaultValue) {
         //temporary tweak to make it work
-        path=path.replaceAll("_", "/");
+        path = path.replaceAll("_", "/");
         try {
             return myself.prefs.getOnionString(path);
         } catch (OnionWrongTypeException | OnionNoEntryException ex) {
@@ -143,17 +195,16 @@ public class Settings {
             return defaultValue;
         }
     }
-    
-    
-    public static void setString( String path, String value){
+
+    public static void setString(String path, String value) {
         //temporary tweak to make it work
-        path=path.replaceAll("_", "/");
+        path = path.replaceAll("_", "/");
         myself.prefs.setValue(path, value);
     }
- 
-    public static boolean getBoolean( String path, boolean defaultValue){
+
+    public static boolean getBoolean(String path, boolean defaultValue) {
         //temporary tweak to make it work
-        path=path.replaceAll("_", "/");
+        path = path.replaceAll("_", "/");
         try {
             return myself.prefs.getOnionBoolean(path);
         } catch (OnionWrongTypeException | OnionNoEntryException ex) {
@@ -161,16 +212,17 @@ public class Settings {
             return defaultValue;
         }
     }
-    
-    
-    public static void setBoolean( String path, boolean value){
+
+    public static void setBoolean(String path, boolean value) {
         //temporary tweak to make it work
-        path=path.replaceAll("_", "/");
+        path = path.replaceAll("_", "/");
         myself.prefs.setValue(path, value);
     }
-    
-    public static void savePreferences(){
+
+    public static void savePreferences() {
+        pref.put("json", prefs.toString());
         Core.getSingleInstance().getSystemIF().savePreferences(FT_PROPS,
                 OOBDConstants.AppPrefsFileName, pref);
     }
+
 }
