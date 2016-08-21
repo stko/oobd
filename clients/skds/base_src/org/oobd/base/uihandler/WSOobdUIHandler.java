4b839ed2ed2dd8a379f1abd680345d227ef9267c
Steffen Köhler <steffen@koehlers.de>
2016-08-21 13:40:45 +0200

    json editor now in settings.html, editor scheme comes from webserver

--- clients/skds/base_src/org/oobd/base/uihandler/WSOobdUIHandler.java
+++ clients/skds/base_src/org/oobd/base/uihandler/WSOobdUIHandler.java
@@ -907,7 +907,11 @@ class OOBDHttpServer extends NanoHTTPD {
             catalog += "</catalog>";
             return newFixedLengthResponse(Response.Status.OK, "text/xml",
                     catalog);
-        } else {
+        } 
+        else if ("/settings.json".equals(session.getUri())){
+            return newFixedLengthResponse(Response.Status.OK, "text/xml",
+                    "{\"properties\":{\"make\":{\"type\":\"string\",\"enum\":[\"Tooooyota\",\"BMW\",\"Honda\",\"Ford\",\"Chevy\",\"VW\"]},\"model\":{\"type\":\"string\"},\"year\":{\"type\":\"integer\",\"enum\":[1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014],\"default\":2008}}}");
+        }else {
             InputStream myFileStream = Core
                     .getSingleInstance()
                     .getSystemIF()
