5a3899ae1017881e5c5861a8fb3f5fede30a37a4
Steffen Köhler <steffen@koehlers.de>
2017-10-15 19:53:46 +0200

    kadaver deamon with dynamic firmware port allocation and -deallocation on closed connections starts to work

--- testing/carCAN_Simulator/kadaverSim.py
+++ testing/carCAN_Simulator/kadaverSim.py
@@ -17,6 +17,8 @@ class kadaverSim(object):
 	def __init__(self,wsURL,connectID, host, port):
 
 		def on_error(ws, error):
+			time.sleep(2)
+			print ("### socket error -wait 2 secs ###")
 			print (error)
 
 		def on_close(ws):
@@ -25,7 +27,7 @@ class kadaverSim(object):
 		def on_open(ws):
 			print ("### connected ###")
 			try:
-				self.send_kadaver_message("OOBD Rocks")
+				send_kadaver_message(self,"OOBD Rocks")
 				print ("### connected2 ###")
 			except Exception as n:
 				print "Exception: " , n
@@ -56,9 +58,10 @@ class kadaverSim(object):
 				if "status" in thisMsg:
 					print ("disconnect from client")
 					if thisMsg["status"]=="disconnect":
-						self.close_serial()
+						close_serial(self)
 			except Exception as n:
-				print "Exception: " , n
+				print "on_message Exception: " , n
+				del(self._serPortSocket)
 
 
 		def serialThread(*args):
@@ -66,7 +69,8 @@ class kadaverSim(object):
 			while True:
 				try:
 					self._serPortSocket
-					socket_list = [sys.stdin, self._serPortSocket]
+					#socket_list = [sys.stdin, self._serPortSocket]
+					socket_list = [ self._serPortSocket]
 						
 					# Get the list sockets which are readable
 					read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
@@ -77,17 +81,26 @@ class kadaverSim(object):
 						print ('Got-1:')
 						if sock == self._serPortSocket:
 							print ('Got-2:')
-							data = sock.recv(4096)
-							if data : # otherways the socket would be closed
-								sys.stdout.write(data)
-								print ('Got:', data)
-								self.send_kadaver_message(data)
+							try:
+								data = sock.recv(4096)
+								if data : # otherways the socket would be closed
+									sys.stdout.write(data)
+									print ('Got:', data)
+									send_kadaver_message(self,data)
+								else:
+									close_serial(self)
+									print "no data -serial socket lost, close it"
+							except:
+								close_serial(self)
+								print "data read exception, close socket"
+								
 						else :
 							#msg = sys.stdin.readline()
 							#_serPortSocket.send(msg)
 							pass
 				except:
-					time.sleep(0.1)
+					print "is serial socket closed?"
+					time.sleep(0.5)
 
 
 		def close_kadaver(self):
@@ -107,65 +120,70 @@ class kadaverSim(object):
 				except Exception as e:
 					raise AssertionError("something went wrong :-("+repr(e)+"\n")
 			
+
+		def close_serial(self):
+			try:
+				self._serPortSocket
+				#shutdown the socket. without shutdown the serial thread would still use the socket and keeps it open
+				self._serPortSocket.shutdown(socket.SHUT_RDWR)
+				self._serPortSocket.close()
+				print ("close dongle telnet")
+				del(self._serPortSocket)
+			except:
+				print("Can't close serial socket")
+				del(self._serPortSocket)
+				pass
+
+
+		
+		def send_kadaver_message(self,message):
+			thisMsg=loads('{ "reply" : "" , "channel" : "" }')
+			thisMsg["reply"]=b64encode(message)
+			thisMsg["channel"]=b64encode(self._connectID)
+			pprint (thisMsg)
+			_flush(self)
+			if self._wsSocket is None:
+				raise AssertionError("send_kadaver_message: Websocket is not open!")
+			self._wsSocket.send(dumps(thisMsg))      # write a string
+			sys.stderr.write("send msg:"+dumps(thisMsg))
+				
+		def _flush(self):
+			if self._wsSocket is None:
+				raise AssertionError("_flush: Websocket is not open!")
+			else:
+				try:
+
+					s="dummy"
+					while s != "":
+						s= self._wsSocket.recv()
+				except Exception as e:
+						pass
+						#raise AssertionError("flush error :-("+repr(e)+"\n")
+
 		### start the init- code itself	
 		self._wsURL=wsURL
 		self._connectID=connectID
-		sys.stderr.write("open "+wsURL+"\n")
-		self._wsSocket = websocket.WebSocketApp(wsURL,
-			on_message = on_message,
-			on_error = on_error,
-			on_close = on_close)
-		if self._wsSocket is None:
-			raise AssertionError("could not open kadaver- Websocket!")
-		
-		self._flush()
-
-
-
-
-
+		sys.stderr.write("starting on "+wsURL+"\n")
 		thread.start_new_thread(serialThread, ())
-		self._wsSocket.on_open = on_open
-		# disable SSL cert check finally found on https://pypi.python.org/pypi/websocket-client/
-		# maybe I should just read the original documentaion more often first before googeling around ;-)
-		self._wsSocket.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
-
-	def close_serial(self):
-		try:
-			self._serPortSocket
-			self._serPortSocket.close()
-			print ("close dongle telnet")
-			del(self._serPortSocket)
-		except:
-			print("Can't close serial socket")
-			pass
+		while (True):
+			self._wsSocket = websocket.WebSocketApp(wsURL,
+				on_message = on_message,
+				on_error = on_error,
+				on_close = on_close)
+			if self._wsSocket is None:
+				#raise AssertionError("could not open kadaver- Websocket!")
+				time.sleep(2)
+				continue
+			_flush(self)
 
 
-	
-	def send_kadaver_message(self,message):
-		thisMsg=loads('{ "reply" : "" , "channel" : "" }')
-		thisMsg["reply"]=b64encode(message)
-		thisMsg["channel"]=b64encode(self._connectID)
-		pprint (thisMsg)
-		self._flush()
-		if self._wsSocket is None:
-			raise AssertionError("send_kadaver_message: Websocket is not open!")
-		self._wsSocket.send(dumps(thisMsg))      # write a string
-		sys.stderr.write("send msg:"+dumps(thisMsg))
-			
-	def _flush(self):
-		if self._wsSocket is None:
-			raise AssertionError("_flush: Websocket is not open!")
-		else:
-			try:
 
-				s="dummy"
-				while s != "":
-					s= self._wsSocket.recv()
-			except Exception as e:
-					pass
-					#raise AssertionError("flush error :-("+repr(e)+"\n")
 
+
+			self._wsSocket.on_open = on_open
+			# disable SSL cert check finally found on https://pypi.python.org/pypi/websocket-client/
+			# maybe I should just read the original documentaion more often first before googeling around ;-)
+			self._wsSocket.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
 		
 		
 			
@@ -193,6 +211,7 @@ def main(argv):
 	print('wsHost is "', wsHost ,'"')
 	print('telnet host is "', telnetHost ,'"')
 	print('telnet Port is "', telnetPort ,'"')
+	# will never return from this...:
 	mySocket= kadaverSim( wsHost, connectID , telnetHost, telnetPort)
 
 
